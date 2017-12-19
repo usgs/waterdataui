015abd4b7a9bbea0de16c808453b22e004a06156
Andrew Yan <ayan@usgs.gov>
2017-12-19 15:07:34 -0600

    instance config.py may not be present if nothing needs to be overridden

--- waterdata/__init__.py
+++ waterdata/__init__.py
@@ -7,6 +7,9 @@ app = Flask(__name__.split()[0], instance_relative_config=True)
 
 # Loads configuration information from config.py and instance/config.py
 app.config.from_object('config')
-app.config.from_pyfile('config.py')
+try:
+    app.config.from_pyfile('config.py')
+except FileNotFoundError:
+    pass
 
 from . import views
