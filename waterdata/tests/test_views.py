250966cc072062dbfa6680d8e4ee3ea6efaceb84
Daniel Naab <dnaab@usgs.gov>
2017-12-29 13:31:26 -0600

    Don't require docstrings in functions/classes that have "test" in their name.

--- waterdata/tests/test_views.py
+++ waterdata/tests/test_views.py
@@ -9,13 +9,10 @@ from ..views import __version__
 
 
 class TestHomeView(TestCase):
-    """Basic tests validating existence of pages."""
-
     def setUp(self):
         self.app_client = app.test_client()
 
     def test_version(self):
-        """Verify that the version number is in the home page response."""
         response = self.app_client.get('/')
 
         self.assertEqual(response.status_code, 200)
