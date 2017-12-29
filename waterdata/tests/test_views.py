f29ca6d37cb5ad26951b9637c3884518ecb0b039
Daniel Naab <dnaab@usgs.gov>
2017-12-29 12:48:12 -0600

    Clear linting warnings from our .pylintrc.

--- waterdata/tests/test_views.py
+++ waterdata/tests/test_views.py
@@ -1,15 +1,21 @@
+"""
+Unit tests for the main WDFN views.
+"""
 
 from unittest import TestCase
 
 from .. import app
 from ..views import __version__
 
+
 class TestHomeView(TestCase):
+    """Basic tests validating existence of pages."""
 
     def setUp(self):
         self.app_client = app.test_client()
 
     def test_version(self):
+        """Verify that the version number is in the home page response."""
         response = self.app_client.get('/')
 
         self.assertEqual(response.status_code, 200)
