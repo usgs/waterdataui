9d5cf6dffd9d6d79c0a81ed552d891dbca81a592
Janell Fry <janell_fry@usgs.gov>
2018-01-04 15:16:15 -0500

    WDFN-14 Added new dictionary for mapping station field names to descriptions and added to table

--- waterdata/views.py
+++ waterdata/views.py
@@ -7,6 +7,8 @@ from flask import render_template
 from . import app, __version__
 from .utils import execute_get_request, parse_rdb
 
+# Station Fields Mapping to Descriptions
+from .constants import STATION_FIELDS_D
 
 SERVICE_ROOT = app.config['SERVICE_ROOT']
 
@@ -34,7 +36,9 @@ def monitoring_location(site_no):
         iter_data = parse_rdb(resp.iter_lines(decode_unicode=True))
         station_record = next(iter_data)
         template = 'monitoring_location.html'
-        context = {'status_code': status, 'station': station_record}
+        context = {'status_code'       : status,
+                   'station'           : station_record,
+                   'STATION_FIELDS_D'  : STATION_FIELDS_D}
         http_code = 200
     elif 400 <= status < 500:
         template = 'monitoring_location.html'
