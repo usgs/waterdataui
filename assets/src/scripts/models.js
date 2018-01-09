463b6b5f6a8d8f24b3fbc671388d1a2c8ee21392
Daniel Naab <dnaab@usgs.gov>
2018-01-09 16:08:35 -0600

    For ajax requests, use XMLHttpRequest rather than a library. This saves us a lot of bytes on the distributable size, and also works cleanly with jasmin-ajax.

--- assets/src/scripts/models.js
+++ assets/src/scripts/models.js
@@ -1,4 +1,3 @@
-const request = require('request');
 const { timeFormat } = require('d3-time-format');
 
 
@@ -7,36 +6,54 @@ const SERVICE_ROOT = 'https://waterservices.usgs.gov/nwis';
 // Create a time formatting function from D3's timeFormat
 const formatTime = timeFormat('%c %Z');
 
-
-export function getTimeseries({sites, params}, callback) {
-    params = params || ['00060'];
-    request.get(
-        `${SERVICE_ROOT}/iv/?sites=${sites.join(',')}&parameterCd=${params.join(',')}&period=P7D&indent=on&siteStatus=all&format=json`,
-        {json: true},
-        (error, response, body) => {
-            if (error) {
-                callback(null, error);
-                return;
+/**
+ * Simple XMLHttpRequest wrapper.
+ * @param  {String}   url      URL to retrieve
+ * @param  {Function} callback Callback function to call with (data, error)
+ */
+function get(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                callback(null, err.message);
             }
-            if (response.statusCode != 200) {
-                callback(null, 'Unexpected error');
-                return;
-            }
-            // Pass timeseries data to callback.
-            callback(body.value.timeSeries.map(series => {
-                return {
-                    code: series.variable.variableCode[0].variableID,
-                    description: series.variable.variableDescription,
-                    values: series.values[0].value.map(value => {
-                        let date = new Date(value.dateTime);
-                        return {
-                            time: date,
-                            value: parseInt(value.value),
-                            label: `${formatTime(date)}\n${value.value} ${series.variable.unit.unitCode}`
-                        };
-                    })
-                };
-            }));
+            callback(data);
         }
-    );
+    };
+
+    xmlhttp.open('GET', url, true);
+    xmlhttp.send();
+}
+
+
+/**
+ * Get a given timeseries dataset from Water Services.
+ * @param  {Array}    options.sites  Array of site IDs to retrieve.
+ * @param  {Array}    options.params List of parameter codes
+ * @param  {Function} callback       Callback to be called with (data, error)
+ */
+export function getTimeseries({sites, params=['00060']}, callback) {
+    let url = `${SERVICE_ROOT}/iv/?sites=${sites.join(',')}&parameterCd=${params.join(',')}&period=P7D&indent=on&siteStatus=all&format=json`;
+    get(url, (data, error) => {
+        if (error) {
+            callback(null, error || 'Unexpected error');
+        }
+        callback(data.value.timeSeries.map(series => {
+            return {
+                code: series.variable.variableCode[0].value,
+                description: series.variable.variableDescription,
+                values: series.values[0].value.map(value => {
+                    let date = new Date(value.dateTime);
+                    return {
+                        time: date,
+                        value: parseInt(value.value),
+                        label: `${formatTime(date)}\n${value.value} ${series.variable.unit.unitCode}`
+                    };
+                })
+            };
+        }));
+    });
 }
