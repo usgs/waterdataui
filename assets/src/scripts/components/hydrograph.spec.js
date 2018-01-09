0148309da8318679ab7df5293cc4a8c74400eedb
Daniel Naab <dnaab@usgs.gov>
2018-01-09 11:18:52 -0600

    Add simple tests that confirm rendering paths hit.

--- assets/src/scripts/components/hydrograph.spec.js
+++ assets/src/scripts/components/hydrograph.spec.js
@@ -1,14 +1,18 @@
 import Hydrograph from './hydrograph';
 
 
-describe('Hydrograph should do stuff', function () {
-    let graph;
-
-    beforeEach(() => {
-        graph = new Hydrograph({});
+describe('Hydrograph charting module', function () {
+    it('empty graph displays warning', () => {
+        let graph = new Hydrograph({});
+        expect(graph._element.innerHTML).toContain('No data is available');
     });
 
-    it('graph exists', () => {
-        expect(graph).not.toBe(null);
+    it('single data point renders', () => {
+        let graph = new Hydrograph({data: [{
+            time: new Date(),
+            value: 10,
+            label: 'Label'
+        }]});
+        expect(graph._element.innerHTML).toContain('hydrograph-container');
     });
 });
