f5da4997dac4af784497b5bd99b0a215fba35083
Andrew Yan <ayan@usgs.gov>
2018-01-29 09:34:18 -0600

    rejigger tests

--- assets/src/scripts/models.spec.js
+++ assets/src/scripts/models.spec.js
@@ -1,5 +1,7 @@
 let proxyquire = require('proxyquireify')(require);
 
+const { parseRDB, parseMedianData, getSiteStatistics } = require('./models');
+
 
 describe('Models module', () => {
 
@@ -137,7 +139,48 @@ describe('Models module', () => {
         })
 
 
-    })
+    });
+
+    describe('parseRDB', () => {
+        it('parseRDB successfully parses RDB content', () => {
+           let result = parseRDB(MOCK_RDB);
+           expect(result.length).toEqual(13);
+           expect(Object.keys(result[0])).toEqual(['agency_cd', 'site_no', 'parameter_cd', 'ts_id', 'loc_web_ds', 'month_nu',
+               'day_nu', 'begin_yr', 'end_yr', 'count_nu', 'p50_va']);
+        });
+
+        it('parseRDB handles no data', () => {
+           let result = parseRDB(MOCK_RDB_NO_DATA);
+           expect(result.length).toEqual(0);
+        });
+
+        it('parseRDB handles no headers', () => {
+           let result = parseRDB(`#Some Stuff`);
+           expect(result.length).toEqual(0);
+        });
+    });
+
+    xdescribe('parseMedianData', () => {
+        it('parseMedian data successfully constructs data for plotting', () => {
+            let result = parseMedianData(MOCK_MEDIAN_DATA, MOCK_TIMESERIES);
+            expect(result.length).toEqual(3);
+            expect(result[0]).toEqual({time: new Date(2017, 7, 5), value: '15'});
+        });
+
+        it('parseMedia data handles empty data', () => {
+            let result = parseMedianData([], []);
+            expect(result.length).toEqual(0);
+        });
+
+        it('parseMedia data handles empty data', () => {
+            let result = parseMedianData([], []);
+            expect(result.length).toEqual(0);
+        });
+    });
+
+    describe('getSiteStatistics', () => {
+
+    });
 });
 
 const MOCK_LAST_YEAR_DATA = `
@@ -3113,3 +3156,103 @@ const MOCK_DATA = `
 "typeSubstituted" : false
 }
 `;
+
+
+const MOCK_RDB = `#
+#
+# US Geological Survey, Water Resources Data
+# retrieved: 2018-01-25 16:05:49 -05:00	(natwebsdas01)
+#
+# This file contains USGS Daily Statistics
+#
+# Note:The statistics generated are based on approved daily-mean data and may not match those published by the USGS in official publications.
+# The user is responsible for assessment and use of statistics from this site.
+# For more details on why the statistics may not match, visit http://help.waterdata.usgs.gov/faq/about-statistics.
+#
+# Data heading explanations.
+# agency_cd       -- agency code
+# site_no         -- Site identification number
+# parameter_cd    -- Parameter code
+# station_nm      -- Site name
+# loc_web_ds      -- Additional measurement description
+#
+# Data for the following 1 site(s) are contained in this file
+# agency_cd   site_no      parameter_cd   station_nm (loc_web_ds)
+# USGS        05370000     00060          EAU GALLE RIVER AT SPRING VALLEY, WI
+#
+# Explanation of Parameter Codes
+# parameter_cd	Parameter Name
+# 00060         Discharge, cubic feet per second
+#
+# Data heading explanations.
+# month_nu    ... The month for which the statistics apply.
+# day_nu      ... The day for which the statistics apply.
+# begin_yr    ... First water year of data of daily mean values for this day.
+# end_yr      ... Last water year of data of daily mean values for this day.
+# count_nu    ... Number of values used in the calculation.
+# p50_va      ... 50 percentile (median) of daily mean values for this day.
+#
+agency_cd	site_no	parameter_cd	ts_id	loc_web_ds	month_nu	day_nu	begin_yr	end_yr	count_nu	p50_va
+5s	15s	5s	10n	15s	3n	3n	6n	6n	8n	12s
+USGS	05370000	00060	153885		1	1	1969	2017	49	16
+USGS	05370000	00060	153885		1	2	1969	2017	49	16
+USGS	05370000	00060	153885		1	3	1969	2017	49	16
+USGS	05370000	00060	153885		1	4	1969	2017	49	15
+USGS	05370000	00060	153885		1	5	1969	2017	49	15
+USGS	05370000	00060	153885		1	6	1969	2017	49	15
+USGS	05370000	00060	153885		1	7	1969	2017	49	15
+USGS	05370000	00060	153885		1	8	1969	2017	49	15
+USGS	05370000	00060	153885		1	9	1969	2017	49	15
+USGS	05370000	00060	153885		1	10	1969	2017	49	15
+USGS	05370000	00060	153885		1	11	1969	2017	49	15
+USGS	05370000	00060	153885		1	12	1969	2017	49	15
+USGS	05370000	00060	153885		1	13	1969	2017	49	15
+`;
+
+const MOCK_RDB_NO_DATA = `#
+#
+# US Geological Survey, Water Resources Data
+# retrieved: 2018-01-25 16:05:49 -05:00	(natwebsdas01)
+#
+# This file contains USGS Daily Statistics
+#
+# Note:The statistics generated are based on approved daily-mean data and may not match those published by the USGS in official publications.
+# The user is responsible for assessment and use of statistics from this site.
+# For more details on why the statistics may not match, visit http://help.waterdata.usgs.gov/faq/about-statistics.
+#
+# Data heading explanations.
+# agency_cd       -- agency code
+# site_no         -- Site identification number
+# parameter_cd    -- Parameter code
+# station_nm      -- Site name
+# loc_web_ds      -- Additional measurement description
+#
+# Data for the following 1 site(s) are contained in this file
+# agency_cd   site_no      parameter_cd   station_nm (loc_web_ds)
+# USGS        05370000     00060          EAU GALLE RIVER AT SPRING VALLEY, WI
+#
+# Explanation of Parameter Codes
+# parameter_cd	Parameter Name
+# 00060         Discharge, cubic feet per second
+#
+# Data heading explanations.
+# month_nu    ... The month for which the statistics apply.
+# day_nu      ... The day for which the statistics apply.
+# begin_yr    ... First water year of data of daily mean values for this day.
+# end_yr      ... Last water year of data of daily mean values for this day.
+# count_nu    ... Number of values used in the calculation.
+# p50_va      ... 50 percentile (median) of daily mean values for this day.
+#
+agency_cd	site_no	parameter_cd	ts_id	loc_web_ds	month_nu	day_nu	begin_yr	end_yr	count_nu	p50_va
+`;
+
+const MOCK_MEDIAN_DATA = [
+    {agency_cd: 'USGS', site_no: '05370000', parameter_cd: '00060', ts_id: '153885', loc_web_ds: '', month_nu: '1', day_nu: '1', begin_yr: '1969', end_yr: '2017', count_nu: '49', p50_va: '16'},
+    {agency_cd: 'USGS', site_no: '05370000', parameter_cd: '00060', ts_id: '153885', loc_web_ds: '', month_nu: '1', day_nu: '13', begin_yr: '1969', end_yr: '2017', count_nu: '49', p50_va: '15'},
+    {agency_cd: 'USGS', site_no: '05370000', parameter_cd: '00060', ts_id: '153885', loc_web_ds: '', month_nu: '8', day_nu: '5', begin_yr: '1969', end_yr: '2017', count_nu: '49', p50_va: '15'}
+];
+
+const MOCK_TIMESERIES = [
+    {'time': new Date(2018, 0, 10), 'value': 21.02},
+    {'time': new Date(2018, 0, 13), 'value': 22.15}
+];
