f23f41fc093b3b6f617c644d29924549fa777c50
Andrew Yan <ayan@usgs.gov>
2018-02-08 13:42:12 -0600

    add median statistics to screen reader table

--- assets/src/scripts/components/hydrograph/index.js
+++ assets/src/scripts/components/hydrograph/index.js
@@ -218,7 +218,25 @@ const timeSeriesGraph = function (elem) {
             points => points.map((value) => {
                 return [value.value, value.time];
             })
-        )
+        ),
+        describeById: () => {return 'time-series-sr-desc'},
+        describeByText: () => {return 'current time series data in tabular format'}
+    })));
+
+    elem.append('div')
+        .call(link(addSROnlyTable, createStructuredSelector( {
+        columnNames: createSelector(
+            (state) => state.title,
+            (title) => [`Median ${title}`, 'Time']
+        ),
+        data: createSelector(
+            pointsSelector('medianStatistics'),
+            points => points.map((value) => {
+                return [value.value, value.time];
+            })
+        ),
+        describeById: () => {return 'median-statistics-sr-desc'},
+        describeByText: () => {return 'median statistical data in tabular format'}
     })));
 };
 
