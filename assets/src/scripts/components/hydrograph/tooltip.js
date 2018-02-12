4b85cf19bb9b8798b1c2d6f36b44b164b1ef569b
Mary Bucknell <mbucknell@usgs.gov>
2018-02-12 11:46:00 -0600

    Added some more code to tooltip.js

--- assets/src/scripts/components/hydrograph/tooltip.js
+++ assets/src/scripts/components/hydrograph/tooltip.js
@@ -3,8 +3,30 @@ const createFocusLine = function(elem, id) {
     let focus = elem.append('g')
         .attr('id', id)
         .attr('class', 'focus')
-        .style('display', 'none')
+        .style('display', 'none');
     focus.append('line')
         .attr('class', 'focus-line');
     return focus;
+};
+
+const createFocusCircle = function(elem, id) {
+    let focus = elem.append('g')
+        .attr('id', id)
+        .attr('class', 'focus')
+        .style('display', 'none')
+    focus.append('circle')
+        .attr('r', 5.5);
+    return focus;
+};
+
+const createTooltipText = function(elem, id, tskeys) {
+    let tooltipTextGroup = elem.append('g')
+        .attr('id', id)
+        .style('display', 'none');
+    for (let tskey of tskeys) {
+        tooltipTextGroup.append('text')
+            .attr('class', `${tskey}-tooltip-text`);
+    }
+    return tooltipTextGroup;
+
 };
