9a69fc2b2325221f46c21041fecf2b3d623b8462
Mary Bucknell <mbucknell@usgs.gov>
2018-03-06 15:21:42 -0600

    Added aria tags to slider and output tag

--- assets/src/scripts/components/floodSlider.js
+++ assets/src/scripts/components/floodSlider.js
@@ -8,25 +8,33 @@ const updateSlider = function(node, {stages, gageHeight}) {
     if (stages.length === 0) {
         node.property('hidden', true);
     } else {
+        const heightIndex = stages.indexOf(gageHeight);
         node.select('input')
             .attr('min', 0)
             .attr('max', stages.length - 1)
             .attr('step', 1)
-            .attr('value', stages.indexOf(gageHeight));
+            .attr('value', heightIndex)
+            .attr('aria-valuemin', 0)
+            .attr('aria-valuemax', stages.length - 1)
+            .attr('aria-valuenow', heightIndex)
+            .attr('aria-valuetext', gageHeight);
         node.select('.range-value').text(gageHeight);
         node.property('hidden', false);
     }
 };
 
 const floodSlider = function(node) {
+    const SLIDER_ID = 'fim-slider'
     let sliderContainer = node.append('div')
         .attr('class', '.slider-wrapper')
         .property('hidden', true);
-    sliderContainer.append('div')
-        .html('Gage Height: <span class="range-value"></span> ft');
+    sliderContainer.append('label')
+        .attr('for', SLIDER_ID)
+        .html(`Gage Height: <output class="range-value" for="${SLIDER_ID}"></output> ft`);
     sliderContainer.append('input')
         .attr('type', 'range')
         .attr('class', 'wdfn-slider')
+        .attr('id', SLIDER_ID)
         .on('change', dispatch(function() {
             return Actions.setGageHeight(this.value);
         }));
