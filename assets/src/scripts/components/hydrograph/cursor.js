a7c0ae9d170f709be40bab2f9031494fa6384058
Daniel Naab <dnaab@usgs.gov>
2018-03-19 12:43:35 -0500

    Tweak slider positioning to line up with graph

--- assets/src/scripts/components/hydrograph/cursor.js
+++ assets/src/scripts/components/hydrograph/cursor.js
@@ -1,10 +1,11 @@
 const { layoutSelector, MARGIN } = require('./layout');
-const { xScaleSelector, yScaleSelector } = require('./scales');
+const { xScaleSelector } = require('./scales');
 
 const { Actions } = require('../../store');
 const { dispatch, link } = require('../../lib/redux');
 
 const SLIDER_STEPS = 1000;
+const SLIDER_OFFSET_PX = 10;
 
 
 export const cursorLocationSelector = state => state.tooltipFocusTime.current;
@@ -16,7 +17,7 @@ export const cursorSlider = function (elem) {
             wrap.append('input')
                 .attr('type', 'range')
                 .attr('id', 'cursor-slider')
-                .attr('left', MARGIN.left)
+                .style('left', MARGIN.left - SLIDER_OFFSET_PX + 'px')
                 .on('input', dispatch(function () {
                     return Actions.setCursorLocation(new Date(this.valueAsNumber));
                 }))
@@ -37,7 +38,7 @@ export const cursorSlider = function (elem) {
                         .classed('active', cursorLocation !== null);
                 }, cursorLocationSelector))
                 .call(link((input, layout) => {
-                    input.attr('width', layout.width - (MARGIN.left + MARGIN.right));
+                    input.style('width', layout.width - MARGIN.right + SLIDER_OFFSET_PX * 2 + 'px');
                 }, layoutSelector));
-        })
+        });
 };
