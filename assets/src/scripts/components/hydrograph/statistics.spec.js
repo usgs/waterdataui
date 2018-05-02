2ab035f944bcd8c7b4e1e4633c8206512a6725b4
Andrew Yan <ayan@usgs.gov>
2018-05-02 09:39:47 -0500

    write tests for making median data look like a time series

--- assets/src/scripts/components/hydrograph/statistics.spec.js
+++ assets/src/scripts/components/hydrograph/statistics.spec.js
@@ -1,124 +1,96 @@
+const { range } = require('lodash');
 const { coerceStatisticalSeries } = require('./statistics');
 
 
 describe('Statistics module', () => {
 
     describe('coerceStatisticalSeries', () => {
-        const points = [
-            {
+        // generate a year's worth of fake median data
+        const MONTHS = range(0, 12);
+        const DAYS = range(1, 32);
+        const YEAR = 2016;
+        let time = [];
+        MONTHS.forEach(month => {
+            DAYS.forEach(day => {
+                let someDay = new Date(YEAR, month, day).getTime();
+                if (!time.includes(someDay)) {
+                    time.push(someDay);
+                }
+            });
+        });
+
+        const dates = time.map(t => new Date(t));
+        const points = dates.map(dt => {
+            let month = dt.getMonth();
+            let day = dt.getDate();
+            return {
                 dateTime: null,
-                month: 0,
-                day: 1,
-                value: 16
-            },
-            {
-                dateTime: null,
-                month: 0,
-                day: 17,
-                value: 85
-            },
-            {
-                dateTime: null,
-                month: 0,
-                day: 18,
-                value: 86
-            },
-            {
-                dateTime: null,
-                month: 1,
-                day: 28,
-                value: 85
-            },
-            {
-                dateTime: null,
-                month: 1,
-                day: 29,
-                value: 41
-            },
-            {
-                dateTime: null,
-                month: 2,
-                day: 5,
-                value: 91
-            },
-            {
-                dateTime: null,
-                month: 2,
-                day: 6,
-                value: 94
-            },
-            {
-                dateTime: null,
-                month: 11,
-                day: 31,
-                value: 42
-            }
-        ];
+                month: month,
+                day: day,
+                value: month + day*2
+            };
+        });
         const series1 = {
             points: points,
-            startTime: new Date(2015, 0, 18),
-            endTime: new Date(2015, 2, 6)
+            endTime: new Date(2015, 2, 3)
         };
 
         const series2 = {
             points: points,
-            startTime: new Date(2014, 0, 1),
-            endTime: new Date(2018, 5, 2)
+            endTime: new Date(2016, 2, 3)
         };
 
         const series3 = {
             points: points,
-            startTime: new Date(2014, 0, 17, 14, 5),
             endTime: new Date(2018, 2, 6, 19, 26)
         };
 
-        const series4 = {
-            points: points,
-            startTime: new Date(2014, 11, 31, 19, 32),
-            endTime: new Date(2015, 0, 10, 18, 20)
-        };
-
-        it('handles coercion to a single year of data', () => {
-            let result = coerceStatisticalSeries(series1);
-            expect(result.length).toEqual(4);
-            expect(result[0]).toEqual({
-                dateTime: new Date(2015, 0, 18),
-                month: 0,
-                day: 18,
-                value: 86
+        it('handles coercion to a single year of data without a leap day', () => {
+            let result = coerceStatisticalSeries(series1, 'P7D');
+            expect(result.length).toEqual(8);
+            result.forEach(datum => {
+                expect(datum.dateTime.getFullYear()).toEqual(2015);
             });
-            expect(result[3]).toEqual({
-                dateTime: new Date(2015, 2, 6),
-                month: 2,
-                day: 6,
-                value: 94
+            expect(result.map(x => x.dateTime.getDate()).includes(29)).toBe(false);
+        });
+
+        it('handles coercion to a single year of data witha leap day', () => {
+            let result = coerceStatisticalSeries(series2, 'P7D');
+            expect(result.length).toEqual(8);
+            result.forEach(datum => {
+                expect(datum.dateTime.getFullYear()).toEqual(2016);
             });
+            expect(result.map(x => x.dateTime.getDate()).includes(29)).toBe(true);
         });
 
-        it('handles coercion of multi-year data', () => {
-            let result = coerceStatisticalSeries(series2);
-            let dateArr = result.map(x => x.dateTime);
-            expect(dateArr).toContain(new Date(2016, 1, 29));
-            expect(dateArr).not.toContain(new Date(2014, 1, 29));
-            expect(dateArr).not.toContain(new Date(2015, 1, 29));
-            expect(dateArr).not.toContain(new Date(2017, 1, 29));
-            expect(dateArr).not.toContain(new Date(2018, 1, 29));
+        it('handles a 30 day period', () => {
+            let result = coerceStatisticalSeries(series1, 'P30D');
+            expect(result.length).toEqual(31);
         });
 
-        it('handles both left and right plot terminus', () => {
-            let result =  coerceStatisticalSeries(series3);
-            const first = result[0];
-            const last = result[result.length - 1];
-            expect(first.dateTime).toEqual(new Date(2014, 0, 17, 14, 5));
-            expect(first.value).toEqual(85);
-            expect(last.dateTime).toEqual(new Date(2018, 2, 6, 19, 26));
-            expect(last.value).toEqual(94);
+        it('handles a non-leap year period', () => {
+            let result = coerceStatisticalSeries(series1, 'P1Y');
+            let years = result.map(x => x.dateTime.getFullYear());
+            expect(result.length).toEqual(366);
+            expect(years.includes(2015)).toBe(true);
+            expect(years.includes(2014)).toBe(true);
         });
 
-        it('handles times around new year well', () => {
-            let result = coerceStatisticalSeries(series4);
-            expect(result.length).toEqual(3);
-            expect(result[0].value).toEqual(42);
+        it('handles a leap year period', () => {
+            let result = coerceStatisticalSeries(series2, 'P1Y');
+            let years = result.map(x => x.dateTime.getFullYear());
+            expect(result.length).toEqual(367);
+            expect(years.includes(2016)).toBe(true);
+            expect(years.includes(2015)).toBe(true);
+        });
+
+        it('handles times in the middle of the day', () => {
+            let result =  coerceStatisticalSeries(series3, 'P7D');
+            expect(result.length).toEqual(9);
+            expect(result[0].dateTime.getTime()).toEqual(new Date(2018, 1, 27, 19, 26).getTime());
+            expect(result[1].dateTime.getTime()).toEqual(new Date(2018, 1, 28).getTime());
+            expect(result[result.length - 2].dateTime.getTime()).toEqual(new Date(2018, 2, 6).getTime());
+            expect(result[result.length - 1].dateTime.getTime()).toEqual(new Date(2018, 2, 6, 19, 26).getTime());
         });
     });
 });
