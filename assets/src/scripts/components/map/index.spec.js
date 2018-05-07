d7ba6267ea7695abc4f49f7d18e9fb5d67e7bccc
Mary Bucknell <mbucknell@usgs.gov>
2018-05-07 10:11:17 -0500

    Render the divs for the flood slider and site map in the html templates.

--- assets/src/scripts/components/map/index.spec.js
+++ assets/src/scripts/components/map/index.spec.js
@@ -4,7 +4,7 @@ const proxyquire = require('proxyquireify')(require);
 const { attachToNode } = require('./index');
 const { configureStore } = require('../../store');
 
-describe('map module', () => {
+fdescribe('map module', () => {
     let mapNode;
     let store;
     let floodDataMock;
@@ -12,9 +12,11 @@ describe('map module', () => {
 
     beforeEach(() => {
         jasmine.Ajax.install();
-        select('body')
+        let mapContainer = select('body')
             .append('div')
                 .attr('id', 'map');
+        mapContainer.append('div').attr('id', 'flood-layer-control-container');
+        mapContainer.append('div').attr('id', 'site-map');
         mapNode = document.getElementById('map');
 
         floodDataMock = {
