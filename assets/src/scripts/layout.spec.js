552435e6f9195889f94a9e41c28a97f6c922f5ee
Daniel Naab <dnaab@usgs.gov>
2018-05-09 14:25:44 -0500

    Add layout/invalidate functions to positioning elements.

---
+++ assets/src/scripts/layout.spec.js
@@ -0,0 +1,54 @@
+const { select } = require('d3-selection');
+
+const { layout, invalidate } = require('./layout');
+
+
+describe('layout', () => {
+    let layoutRoot;
+
+    beforeEach(() => {
+        layoutRoot = select('body').append('div');
+    });
+
+    afterEach(() => {
+        layoutRoot.remove();
+    });
+
+    it('has this bound to current node', () => {
+        layoutRoot.call(layout(function () {
+            expect(this).toBe(layoutRoot.node());
+        }));
+        layoutRoot.call(invalidate());
+    });
+
+    it('invalidation accumulates layout data', () => {
+        let acc;
+        layoutRoot.call(layout(function (data) {
+            expect(data).toBeDefined();
+            acc = data;
+        }));
+        layoutRoot.call(invalidate({'rootInfo': '1'}));
+        layoutRoot.call(invalidate({'rootInfo2': '2'}));
+        layoutRoot.call(invalidate({'rootInfo': '3'}));
+        expect(acc).toEqual({
+            'rootInfo': '3',
+            'rootInfo2': '2'
+        });
+    });
+
+    it('invalidation accumulates layout data on children', () => {
+        let acc;
+        layoutRoot.call(layout(function (data) {
+            expect(data).toBeDefined();
+            acc = data;
+        }));
+        layoutRoot.append('div')
+            .call(invalidate({'child1': '1'}));
+        layoutRoot.append('div')
+            .call(invalidate({'child2': '2'}));
+        expect(acc).toEqual({
+            'child1': '1',
+            'child2': '2'
+        });
+    });
+});
