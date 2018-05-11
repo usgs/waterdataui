7960c2350b12e40d8ddee224f910b49b4210e4e5
Daniel Naab <dnaab@usgs.gov>
2018-05-11 16:48:55 -0500

    Add createEvent function to fix some tests on IE

--- assets/src/scripts/helpers.spec.js
+++ assets/src/scripts/helpers.spec.js
@@ -3,6 +3,27 @@ const { select } = require('d3-selection');
 const { registerTooltips, unregisterTooltips } = require('./helpers');
 
 
+/**
+ * Cross-browser helper for creating events
+ * @param  {String}  eventType
+ * @param  {Boolean} bubbles
+ * @param  {Boolean} cancelable
+ * @return {Event}
+ */
+const createEvent = function (eventType, bubbles = true, cancelable = false) {
+    // Standard method
+    if (typeof window.Event === 'function') {
+        return new window.Event(eventType, {bubbles, cancelable});
+
+    // For Internet Explorer
+    } else {
+        const event = document.createEvent('Event');
+        event.initEvent(eventType, bubbles, cancelable);
+        return event;
+    }
+};
+
+
 describe('helpers module', () => {
     let tooltipItem, child, notATooltip, handler;
 
@@ -25,24 +46,24 @@ describe('helpers module', () => {
     });
 
     it('sets show-tooltip class on child touchstart', () => {
-        child.dispatchEvent(new window.Event('touchstart', {bubbles: true}));
+        child.dispatchEvent(createEvent('touchstart'));
         expect(tooltipItem.classed('show-tooltip')).toBe(true);
     });
 
     it('unsets show-tooltip class on second tooltip click', () => {
-        child.dispatchEvent(new window.Event('touchstart', {bubbles: true}));
-        child.dispatchEvent(new window.Event('touchstart', {bubbles: true}));
+        child.dispatchEvent(createEvent('touchstart'));
+        child.dispatchEvent(createEvent('touchstart'));
         expect(tooltipItem.classed('show-tooltip')).toBe(false);
     });
 
     it('unsets show-tooltip class on click outside tooltip', () => {
-        child.dispatchEvent(new window.Event('touchstart', {bubbles: true}));
-        notATooltip.dispatchEvent(new window.Event('touchstart', {bubbles: true}));
+        child.dispatchEvent(createEvent('touchstart'));
+        notATooltip.dispatchEvent(createEvent('touchstart'));
         expect(tooltipItem.classed('show-tooltip')).toBe(false);
     });
 
     it('does not set show-tooltip class on non-tooltip node', () => {
-        notATooltip.dispatchEvent(new window.Event('touchstart', {bubbles: true}));
+        notATooltip.dispatchEvent(createEvent('touchstart'));
         expect(tooltipItem.classed('show-tooltip')).toBe(false);
     });
 });
