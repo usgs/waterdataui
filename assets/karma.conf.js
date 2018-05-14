f25b1c08c0839469e1bc48a3c1b6d11d061e695e
Daniel Naab <dnaab@usgs.gov>
2018-05-14 09:50:28 -0500

    Add matchMedia polyfill (and remove babel-polyfill from karma config, as it doesn't seem to be fixing errors on IE)

--- assets/karma.conf.js
+++ assets/karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'node_modules/babel-polyfill/dist/polyfill.min.js',
+            //'node_modules/babel-polyfill/dist/polyfill.min.js',
             'tests/scripts/globalConfig.js',
             'src/scripts/**/*.js'
         ],
