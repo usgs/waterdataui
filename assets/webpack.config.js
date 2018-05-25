777d2bea5f2f6329ec8f8834952df3d27f80a573
Daniel Naab <dnaab@usgs.gov>
2018-05-25 14:30:54 -0500

    Minor cleanup

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     module: {
         rules: [{
             test: /\.js$/,
-            //exclude: /node_modules/,
+            exclude: /node_modules/,
             use: {
                 loader: 'babel-loader?cacheDirectory=true&babelrc=true'
             }
