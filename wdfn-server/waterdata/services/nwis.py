03029f7eb70ac7d6c6b98960af978ee05d06e6cf
ayan <ayan@usgs.gov>
2019-03-25 15:59:11 -0500

    add docstrings to the NWIS web service class

--- wdfn-server/waterdata/services/nwis.py
+++ wdfn-server/waterdata/services/nwis.py
@@ -35,13 +35,35 @@ def parse_rdb(rdb_iter_lines):
 
 
 class NwisWebServices:
+    """
+    Access to NWISWeb services required by the waterdataui application
+    to render pages.
+
+    """
 
     def __init__(self, service_root, path='/nwis/site/', data_format='rdb'):
+        """
+        Constructor method.
+
+        :param str service_root: the scheme and host of the NWISWeb service
+        :param str path: the service path to be queried, defaults to `/nwis/site/`
+        :param str data_format: the data format to be returned from the service call
+
+        """
         self.service_root = service_root
         self.path = path
         self.data_format = data_format
 
     def get_site(self, site_no, agency_cd):
+        """
+        Get a the sites owned by an agency.
+
+        :param str site_no: site identifier
+        :param str agency_cd: identifier for the agency that owns the site
+        :return: an object containing the sites requested
+        :rtype: requests.Response
+
+        """
         resp = execute_get_request(
             self.service_root,
             path=self.path,
@@ -55,6 +77,15 @@ class NwisWebServices:
         return resp
 
     def get_site_parameters(self, site_no, agency_cd):
+        """
+        Get the parameters measured at a site.
+
+        :param str site_no: site identifier
+        :param str agency_cd: identifier for the agency that owns the site:
+        :return: parameters that have been measured at a site
+        :rtype: requests.Response
+
+        """
         resp = execute_get_request(
             self.service_root,
             path=self.path,
@@ -69,6 +100,15 @@ class NwisWebServices:
         return resp
 
     def get_huc_sites(self, huc_cd):
+        """
+        Get all sites within a hydrologic unit as identified by its
+        hydrologic unit code (HUC).
+
+        :param str huc_cd: hydrologic unit code
+        :return: all sites in the specified HUC
+        :rtype: requests.Response
+
+        """
         resp = execute_get_request(
             self.service_root,
             path=self.path,
@@ -80,6 +120,14 @@ class NwisWebServices:
         return resp
 
     def get_county_sites(self, state_county_cd):
+        """
+        Get all sites within a county.
+
+        :param str state_county_cd: FIPS ID for a county
+        :return: all sites within the specified county
+        :rtype: requests.Response
+
+        """
         resp = execute_get_request(
             self.service_root,
             path=self.path,
