e4a00bd9e8bd0c888af92e31139a395b78e493ef
ayan <ayan@usgs.gov>
2019-03-26 15:33:04 -0500

    rework tests

--- wdfn-server/waterdata/tests/services/test_nwis.py
+++ wdfn-server/waterdata/tests/services/test_nwis.py
@@ -2,9 +2,11 @@
 Tests for NWISWeb service calls.
 
 """
+from types import GeneratorType
 from unittest import TestCase, mock
 
 from waterdata.services.nwis import NwisWebServices
+from ..rdb_snippets import SITE_RDB, PARAMETER_RDB
 
 
 class TestNwisWebServices(TestCase):
@@ -18,13 +20,13 @@ class TestNwisWebServices(TestCase):
         self.county_code = '055:213'
 
     def test_default_path(self):
-        ns = NwisWebServices(self.service_root)
-        self.assertEqual(ns.path, '/nwis/site/')
+        nwis = NwisWebServices(self.service_root)
+        self.assertEqual(nwis.path, '/nwis/site/')
 
     @mock.patch('waterdata.services.nwis.execute_get_request')
     def test_get_site(self, r_mock):
-        ns = NwisWebServices(self.service_root, self.path)
-        ns.get_site(self.site_no, self.agency_cd)
+        nwis = NwisWebServices(self.service_root, self.path)
+        nwis.get_site(self.site_no, self.agency_cd)
         r_mock.assert_called_with(
             self.service_root,
             path=self.path,
@@ -37,9 +39,14 @@ class TestNwisWebServices(TestCase):
         )
 
     @mock.patch('waterdata.services.nwis.execute_get_request')
-    def test_get_site_parameters(self, r_mock):
+    def test_get_site_parameters_okay(self, r_mock):
+        mock_resp = mock.Mock()
+        mock_resp.status_code = 200
+        mock_resp.iter_lines.return_value = iter(PARAMETER_RDB.split('\n'))
+        r_mock.return_value = mock_resp
+
         nwis = NwisWebServices(self.service_root, self.path)
-        nwis.get_site_parameters(self.site_no, self.agency_cd)
+        data = nwis.get_site_parameters(self.site_no, self.agency_cd)
         r_mock.assert_called_with(
             self.service_root,
             path=self.path,
@@ -51,11 +58,40 @@ class TestNwisWebServices(TestCase):
                 'agencyCd': self.agency_cd
             }
         )
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 8)
 
     @mock.patch('waterdata.services.nwis.execute_get_request')
-    def test_get_huc_sites(self, r_mock):
+    def test_get_site_parameters_not_okay(self, r_mock):
+        mock_resp = mock.Mock()
+        mock_resp.status_code = 503
+        r_mock.return_value = mock_resp
+
         nwis = NwisWebServices(self.service_root, self.path)
-        nwis.get_huc_sites(self.huc_cd)
+        data = nwis.get_site_parameters(self.site_no, self.agency_cd)
+        r_mock.assert_called_with(
+            self.service_root,
+            path=self.path,
+            params={
+                'sites': self.site_no,
+                'format': 'rdb',
+                'seriesCatalogOutput': True,
+                'siteStatus': 'all',
+                'agencyCd': self.agency_cd
+            }
+        )
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 0)
+
+    @mock.patch('waterdata.services.nwis.execute_get_request')
+    def test_get_huc_sites_okay(self, r_mock):
+        mock_resp = mock.Mock()
+        mock_resp.status_code = 200
+        mock_resp.iter_lines.return_value = iter(SITE_RDB.split('\n'))
+        r_mock.return_value = mock_resp
+
+        nwis = NwisWebServices(self.service_root, self.path)
+        data = nwis.get_huc_sites(self.huc_cd)
         r_mock.assert_called_with(
             self.service_root,
             path=self.path,
@@ -64,11 +100,37 @@ class TestNwisWebServices(TestCase):
                 'huc': self.huc_cd
             }
         )
+        self.assertIsInstance(data, GeneratorType)
+        self.assertEqual(len(list(data)), 1)
 
     @mock.patch('waterdata.services.nwis.execute_get_request')
-    def test_get_county_sites(self, r_mock):
+    def test_get_huc_sites_not_okay(self, r_mock):
+        mock_resp = mock.Mock()
+        mock_resp.status_code = 404
+        r_mock.return_value = mock_resp
+
         nwis = NwisWebServices(self.service_root, self.path)
-        nwis.get_county_sites(self.county_code)
+        data = nwis.get_huc_sites(self.huc_cd)
+        r_mock.assert_called_with(
+            self.service_root,
+            path=self.path,
+            params={
+                'format': 'rdb',
+                'huc': self.huc_cd
+            }
+        )
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 0)
+
+    @mock.patch('waterdata.services.nwis.execute_get_request')
+    def test_get_county_sites_okay(self, r_mock):
+        mock_resp = mock.Mock()
+        mock_resp.status_code = 200
+        mock_resp.iter_lines.return_value = iter(SITE_RDB.split('\n'))
+        r_mock.return_value = mock_resp
+
+        nwis = NwisWebServices(self.service_root, self.path)
+        data = nwis.get_county_sites(self.county_code)
         r_mock.assert_called_with(
             self.service_root,
             path=self.path,
@@ -77,3 +139,24 @@ class TestNwisWebServices(TestCase):
                 'countyCd': self.county_code
             }
         )
+        self.assertIsInstance(data, GeneratorType)
+        self.assertEqual(len(list(data)), 1)
+
+    @mock.patch('waterdata.services.nwis.execute_get_request')
+    def test_get_county_sites_not_okay(self, r_mock):
+        mock_resp = mock.Mock()
+        mock_resp.status_code = 500
+        r_mock.return_value = mock_resp
+
+        nwis = NwisWebServices(self.service_root, self.path)
+        data = nwis.get_county_sites(self.county_code)
+        r_mock.assert_called_with(
+            self.service_root,
+            path=self.path,
+            params={
+                'format': 'rdb',
+                'countyCd': self.county_code
+            }
+        )
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 0)
