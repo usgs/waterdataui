4e74bd32519be6a507c796c19581ba6d1ea97062
mbucknell <mbucknell@usgs.gov>
2019-08-29 14:12:05 -0500

    Updated javascript and python dependencies. Also updated version of node to 10.16.3

--- assets/src/scripts/components/map/index.js
+++ assets/src/scripts/components/map/index.js
@@ -1,7 +1,7 @@
 import { select } from 'd3-selection';
 import { createStructuredSelector } from 'reselect';
 import { map as createMap, marker as createMarker, control, layerGroup, icon } from 'leaflet';
-import { TiledMapLayer, dynamicMapLayer, Util, basemapLayer, featureLayer } from 'esri-leaflet/src/EsriLeaflet';
+import { TiledMapLayer, dynamicMapLayer, Util, basemapLayer, featureLayer } from 'esri-leaflet';
 import { link, provide } from '../../lib/redux';
 import config from '../../config';
 import { FLOOD_EXTENTS_ENDPOINT, FLOOD_BREACH_ENDPOINT, FLOOD_LEVEE_ENDPOINT } from '../../flood-data';
