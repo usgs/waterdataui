1f9da073b23533914865e4e2c45910e229bddee5
David Uselmann <duselmann@usgs.gov>
2019-09-13 11:13:51 -0500

    refined unit test
    The initial test was a rough draft and these updates make it work from the command line. Note that to run it it seemed to require the sys.path.append and the import .. notation failed.

--- wdfn-server/waterdata/tests/test_camera.py
+++ wdfn-server/waterdata/tests/test_camera.py
@@ -1,7 +1,10 @@
-import ../camera
 from unittest import TestCase
+import sys
+sys.path.append("..")
+import camera
+# from .. import camera
 
-camera.JSON_CAMERA_FILE = "../data/monitoring_camera_data_test.json"
+camera.JSON_CAMERA_FILE = "../../data/monitoring_camera_data_test.json"
 
 class TestCamera(TestCase):
     def test_loading_json_file(self):
@@ -15,25 +18,30 @@ class TestCamera(TestCase):
         self.assertTrue("22222" in json_raw)
 
     def test_all_camera_dictionary_list(self):
+        json_raw = camera.get_json_camera_data()
         cameras = camera.json_raw_to_dictionary(json_raw)
         # print(len(cameras))
         self.assertEquals(len(cameras), 3)
         self.assertEquals("12345", cameras[1]['SiteId'])
 
     def test_get_site_camera_from_list(self):
-        camera_data1 = camera.find_site_in_cameras('12345', cameras)
-        # print(camera_data1)
-        self.assertEquals("12345", cameras_data1['SiteId'])
+        json_raw = camera.get_json_camera_data()
+        cameras = camera.json_raw_to_dictionary(json_raw)
+        camera_data = camera.find_site_in_cameras('12345', cameras)
+        # print(camera_data)
+        self.assertEquals("12345", camera_data['SiteId'])
 
     def test_get_site_camera_dictionary_by_id(self):
+        json_raw = camera.get_json_camera_data()
+        cameras = camera.json_raw_to_dictionary(json_raw)
         camera_data1 = camera.find_site_in_cameras('12345', cameras)
         camera_data2 = camera.get_site_camera_data('12345')
         # print(camera_data1['SiteId'])
         # print(camera_data2['SiteId'])
         # print(camera_data1['SiteId'] == camera_data2['SiteId'])
-        self.assertEquals("12345", cameras_data1['SiteId'])
-        self.assertEquals("12345", cameras_data2['SiteId'])
-        self.assertEquals(cameras_data2, cameras_data1)
+        self.assertEquals("12345", camera_data1['SiteId'])
+        self.assertEquals("12345", camera_data2['SiteId'])
+        self.assertEquals(camera_data2, camera_data1)
 
     def test_get_site_monitoring_camera_data(self):
         monitoring_camera = camera.get_monitoring_camera_data('12345')
@@ -59,6 +67,7 @@ class TestCamera(TestCase):
         context = {
             'status_code': 200,
         }
+        monitoring_camera = camera.get_monitoring_camera_data('12345')
         if monitoring_camera:
             context['monitoring_camera'] = monitoring_camera
         # print(context)
@@ -70,13 +79,13 @@ class TestCamera(TestCase):
         self.assertTrue("frame_gallery" in context['monitoring_camera'])
 
     def test_code_snipet_injecting_missing_camera_into_context(self):
-        context_missing = {
+        context = {
             'status_code': 200,
         }
         missing_camera = camera.get_monitoring_camera_data('missing')
         if missing_camera:
-            context_missing['monitoring_camera'] = missing_camera
-        # print(context_missing)
+            context['monitoring_camera'] = missing_camera
+        # print(context)
         self.assertTrue("exists" in context['monitoring_camera'])
         self.assertEquals(context['monitoring_camera']["exists"], False)
         self.assertFalse("thumb" in context['monitoring_camera'])
