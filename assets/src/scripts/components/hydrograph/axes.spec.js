c271419b97854e5ff365ec642db71277dac6ca08
Andrew Yan <ayan@usgs.gov>
2019-09-16 12:02:33 -0500

    test axes creation when using custom dates

--- assets/src/scripts/components/hydrograph/axes.spec.js
+++ assets/src/scripts/components/hydrograph/axes.spec.js
@@ -61,9 +61,24 @@ describe('Chart axes', () => {
         const startP7D = 1503610440000;
         const startP30D = 1501623240000;
         const startP1Y = 1472679240000;
+        const startCustomUnderOneDecade = 1274590800000;
+        const startCustomOverTwoDecades = 651906000000;
 
         it('creates tick marks for a 7 day period', () => {
             const result = generateDateTicks(startP7D, endDate, 'P7D', timeZone);
+            expect(result).toEqual([
+                1503644400000,
+                1503730800000,
+                1503817200000,
+                1503903600000,
+                1503990000000,
+                1504076400000,
+                1504162800000
+            ]);
+        });
+
+        it('uses weekly ticks for 7 or fewer days for a custom date range', () => {
+            const result = generateDateTicks(startP7D, endDate, 'custom', timeZone);
             expect(result).toEqual([
                 1503644400000,
                 1503730800000,
@@ -85,6 +100,16 @@ describe('Chart axes', () => {
             ]);
         });
 
+        it('uses weekly ticks for a different of days 7 between 30 for a custom date range', () => {
+            const result = generateDateTicks(startP30D, endDate, 'custom', timeZone);
+            expect(result).toEqual([
+                1502002800000,
+                1502607600000,
+                1503212400000,
+                1503817200000
+            ]);
+        });
+
         it('creates tick marks for a 1 year period', () => {
             const result = generateDateTicks(startP1Y, endDate, 'P1Y', timeZone);
             expect(result.length).toBeGreaterThanOrEqual(6);
@@ -92,6 +117,48 @@ describe('Chart axes', () => {
             expect(result.includes(1480579200000)).toBe(true);
             expect(result.includes(1496300400000)).toBe(true);
             expect(result.includes(1501570800000)).toBe(true);
+        });
+
+        it('custom uses monthly marks for 30 days through a year', () => {
+            const result = generateDateTicks(startP1Y, endDate, 'custom', timeZone);
+            expect(result.length).toBeGreaterThanOrEqual(6);
+            expect(result.includes(1475305200000)).toBe(true);
+            expect(result.includes(1480579200000)).toBe(true);
+            expect(result.includes(1496300400000)).toBe(true);
+            expect(result.includes(1501570800000)).toBe(true);
+        });
+
+        it('custom ticks are correctly generated for dates under one date', () => {
+            const result = generateDateTicks(startCustomUnderOneDecade, endDate, 'custom', timeZone);
+            expect(result).toEqual([
+                1293868800000,
+                1314860400000,
+                1335855600000,
+                1357027200000,
+                1378018800000,
+                1398927600000,
+                1420099200000,
+                1441090800000,
+                1462086000000,
+                1483257600000
+            ]);
+        });
+
+        it('custom ticks use correctly for dates over two decades', () => {
+            const result = generateDateTicks(startCustomOverTwoDecades, endDate, 'custom', timeZone);
+            expect(result).toEqual([
+                723196800000,
+                796723200000,
+                870418800000,
+                944035200000,
+                1017648000000,
+                1091343600000,
+                1164960000000,
+                1238569200000,
+                1312182000000,
+                1385884800000,
+                1459494000000
+            ]);
         });
     });
 });
