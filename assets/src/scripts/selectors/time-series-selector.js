e34cf5ed22b13a279a44dbcfeb5f4de183d0863f
mbucknell <mbucknell@usgs.gov>
2019-09-26 10:06:20 -0500

    Added selectors to get all time series for the current variable and to get all methods for the current variable.

--- assets/src/scripts/selectors/time-series-selector.js
+++ assets/src/scripts/selectors/time-series-selector.js
@@ -112,7 +112,7 @@ export const getTSRequest = memoize((tsKey, period, parmCd) => createSelector(
     (requests, tsRequestKey) => requests[tsRequestKey] || {}
 ));
 
-/*
+ /*
  * @param {String} tsKey - current or compare
  * @param {String} or null period - date range of interest specified as an ISO-8601 duration. If null, P7D is assumed
  * @param {String} or null parmCd - Only need to specify if period is something other than P7D or null
