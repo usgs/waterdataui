730969fcc502f260cfde8eb0a2ccc6e1b42dd347
ssoper <ssoper@usgs.gov>
2019-10-07 13:47:10 -0500

    Adding test of the method-picker hidden property.

--- assets/src/scripts/components/hydrograph/method-picker.spec.js
+++ assets/src/scripts/components/hydrograph/method-picker.spec.js
@@ -59,6 +59,45 @@ describe('method-picker', () => {
             }
         };
 
+        const TEST_STATE_ONE_METHOD = {
+            series: {
+                timeSeries: {
+                    '00010:current': {
+                        points: DATA,
+                        tsKey: 'current:P7D',
+                        variable: '00010id',
+                        method: 69930
+                    },
+                    '00010:compare': {
+                        points: DATA,
+                        tsKey: 'compare:P7D',
+                        variable: '00010id',
+                        method: 69930
+                    }
+                },
+                variables: {
+                    '00010id': {
+                        oid: '00010id',
+                        variableCode: {
+                            value: '00010'
+                        },
+                        unit: {
+                            unitCode: 'deg C'
+                        }
+                    }
+                },
+                methods: {
+                    69930: {
+                        methodDescription: 'Description 1',
+                        methodID: 69930
+                    }
+                }
+            },
+            timeSeriesState: {
+                currentVariableID: '00010id'
+            }
+        };
+
         let div;
         beforeEach(() => {
             div = select('body').append('div');
@@ -74,8 +113,22 @@ describe('method-picker', () => {
             div.call(provide(store))
                 .call(drawMethodPicker);
 
+            expect(div.select('div').property('hidden')).toEqual(false);
+            expect(div.select('select').property('value')).toEqual('69930');
+            expect(store.getState().timeSeriesState.currentMethodID).toEqual(69930);
+        });
+
+        let storeWithOneMethod = configureStore(TEST_STATE_ONE_METHOD);
+
+        it('Creates a picker and sets the hidden property based on the number of methods', () => {
+            div.call(provide(storeWithOneMethod))
+                .call(drawMethodPicker);
+
+            expect(div.select('div').property('hidden')).toEqual(true);
             expect(div.select('select').property('value')).toEqual('69930');
             expect(store.getState().timeSeriesState.currentMethodID).toEqual(69930);
         });
     });
+
+
 });
