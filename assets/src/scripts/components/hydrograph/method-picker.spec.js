2680768e54ec3e373796a98a326298d6ce74f582
ssoper <ssoper@usgs.gov>
2019-10-07 15:07:05 -0500

    Improved tests

--- assets/src/scripts/components/hydrograph/method-picker.spec.js
+++ assets/src/scripts/components/hydrograph/method-picker.spec.js
@@ -59,45 +59,6 @@ describe('method-picker', () => {
             }
         };
 
-        const TEST_STATE_ONE_METHOD = {
-            series: {
-                timeSeries: {
-                    '00010:current': {
-                        points: DATA,
-                        tsKey: 'current:P7D',
-                        variable: '00010id',
-                        method: 69930
-                    },
-                    '00010:compare': {
-                        points: DATA,
-                        tsKey: 'compare:P7D',
-                        variable: '00010id',
-                        method: 69930
-                    }
-                },
-                variables: {
-                    '00010id': {
-                        oid: '00010id',
-                        variableCode: {
-                            value: '00010'
-                        },
-                        unit: {
-                            unitCode: 'deg C'
-                        }
-                    }
-                },
-                methods: {
-                    69930: {
-                        methodDescription: 'Description 1',
-                        methodID: 69930
-                    }
-                }
-            },
-            timeSeriesState: {
-                currentVariableID: '00010id'
-            }
-        };
-
         let div;
         beforeEach(() => {
             div = select('body').append('div');
@@ -107,9 +68,8 @@ describe('method-picker', () => {
             div.remove();
         });
 
-        let store = configureStore(TEST_STATE);
-
         it('Creates a picker and sets the currentMethodID', () => {
+            let store = configureStore(TEST_STATE);
             div.call(provide(store))
                 .call(drawMethodPicker);
 
@@ -118,15 +78,27 @@ describe('method-picker', () => {
             expect(store.getState().timeSeriesState.currentMethodID).toEqual(69930);
         });
 
-        let storeWithOneMethod = configureStore(TEST_STATE_ONE_METHOD);
-
         it('Creates a picker and sets the hidden property based on the number of methods', () => {
+            let TEST_STATE_ONE_METHOD = {
+                ...TEST_STATE,
+                series: {
+                    ...TEST_STATE.series,
+                    timeSeries: {
+                        ...TEST_STATE.series.timeSeries,
+                        '00010:compare': {
+                            ...TEST_STATE.series.timeSeries['00010:compare'],
+                            method: 69930
+                        }
+                    }
+                }
+            };
+            let storeWithOneMethod = configureStore(TEST_STATE_ONE_METHOD);
             div.call(provide(storeWithOneMethod))
                 .call(drawMethodPicker);
 
             expect(div.select('div').property('hidden')).toEqual(true);
             expect(div.select('select').property('value')).toEqual('69930');
-            expect(store.getState().timeSeriesState.currentMethodID).toEqual(69930);
+            expect(storeWithOneMethod.getState().timeSeriesState.currentMethodID).toEqual(69930);
         });
     });
 });
