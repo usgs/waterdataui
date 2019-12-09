822baa8dd1ccdddeae2cd113deb9dac6f1c793d4
mbucknell <mbucknell@usgs.gov>
2019-12-09 13:02:34 -0600

    Revert local changes

--- graph-server/src/renderer/index.js
+++ graph-server/src/renderer/index.js
@@ -2,7 +2,7 @@ const renderPNG = require('./render-png');
 
 const SERVICE_ROOT = process.env.SERVICE_ROOT || 'https://waterservices.usgs.gov/nwis';
 const PAST_SERVICE_ROOT = process.env.PAST_SERVICE_ROOT || 'https://nwis.waterservices.usgs.gov/nwis';
-const STATIC_ROOT = process.env.STATIC_ROOT || 'https://waterdata.usgs.gov/nwisweb/wsgi/stati';
+const STATIC_ROOT = process.env.STATIC_ROOT || 'https://waterdata.usgs.gov/nwisweb/wsgi/static';
 
 
 const renderToRespone = function (res, {siteID, parameterCode, compare}) {
