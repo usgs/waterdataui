0eadba26c49efdbf62961214fffd0c2febc6b549
tomcat service <tomcat@cidasddvaswdfnp.cr.usgs.gov>
2020-02-03 16:09:16 -0600

    Bump version: 0.25.0dev → 0.25.0

--- wdfn-server/setup.py
+++ wdfn-server/setup.py
@@ -79,7 +79,7 @@ def identify_data_files(data_dirs, exclusions=('.gitignore', '.webassets-cache')
 
 setup(
     name='usgs_waterdata_ui',
-    version='0.25.0dev',
+    version='0.25.0',
     description='USGS Water Data',
     author='Mary Bucknell, Andrew Yan, Dan Naab, Janell Fry, Aaron Briggs',
     author_email='mbucknell@usgs.gov',
