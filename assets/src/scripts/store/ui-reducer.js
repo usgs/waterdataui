dae10342158023e56c9c3e8e4e57027f34adcd16
gpetrochenkov-usgs <gpetrochenkov@usgs.gov>
2020-03-13 10:10:01 -0400

    Adjust ui reducer

--- assets/src/scripts/store/ui-reducer.js
+++ assets/src/scripts/store/ui-reducer.js
@@ -10,28 +10,9 @@ const resizeUI = function(ui, action) {
     };
 };
 
-const setHydrographBrushOffset= function(ui, action) {
-    return {
-        ...ui,
-        hydrographBrushOffset: {
-            start: action.hydrographBrushOffset[0],
-            end: action.hydrographBrushOffset[1]
-        }
-    };
-};
-
-const clearHydrographBrushOffset = function(ui) {
-    return {
-        ...ui,
-        hydrographBrushOffset: undefined
-    };
-};
-
 export const uiReducer = function(ui={}, action) {
     switch(action.type) {
         case 'RESIZE_UI': return resizeUI(ui, action);
-        case 'SET_HYDROGRAPH_BRUSH_OFFSET': return setHydrographBrushOffset(ui, action);
-        case 'CLEAR_HYDROGRAPH_BRUSH_OFFSET': return clearHydrographBrushOffset(ui);
         default: return ui;
     }
 };
