282a9253183fe004b2dc69ea8f504914f83110ea
mbucknell <mbucknell@usgs.gov>
2020-04-06 10:50:55 -0500

    Added function comments and eliminated console.log

--- assets/src/scripts/d3-rendering/cursor-slider.js
+++ assets/src/scripts/d3-rendering/cursor-slider.js
@@ -1,10 +1,19 @@
 
 import {sliderTop} from 'd3-simple-slider';
 
+/*
+ * Creates a group in svg offset by layout.margin.left and renders a d3-simple-slider with a
+ * domain and range of xScale. Appropriate event handlers are added that dispatch setCursorOffsetAction
+ * to the redux store when the cursor slider is changed.
+ * @param {D3 svg selection} svg
+ * @param {Number} cursorOffset - Is the difference between the cursor location and the beginning of the scale
+ * @param {Object} layout - the margin.left property is used to offset the slider,
+ * @param {Object} store - Redux store instance
+ * @param {Object} setCursorOffsetAction - Redux action which will be dispatched when the slider value changes.
+ */
 export const drawCursorSlider = function(svg, {cursorOffset, xScale, layout}, store, setCursorOffsetAction) {
     const [startMillis, endMillis] = xScale.domain();
     const [startX, endX] = xScale.range();
-    console.log(`Rerendering slider with cursorOffset ${cursorOffset}`);
     let slider = sliderTop()
         .min(startMillis)
         .max(endMillis)
@@ -22,5 +31,4 @@ export const drawCursorSlider = function(svg, {cursorOffset, xScale, layout}, st
     slider.on('end', (val) => {
         store.dispatch(setCursorOffsetAction(val - startMillis));
     });
-
 };
