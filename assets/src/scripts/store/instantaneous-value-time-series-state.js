a0ff7bf144126e78c8d38a1474058ccd7d5e0db3
mbucknell <mbucknell@usgs.gov>
2020-04-17 15:41:43 -0500

    Refactored the iv data and state into separate files which include the reducers and actions

---
+++ assets/src/scripts/store/instantaneous-value-time-series-state.js
@@ -0,0 +1,267 @@
+/*
+ * Actions for the ivTimeSeriesStateReducer
+ */
+
+/*
+ * Synchronous action to toggle the visibility of specific time series (i.e. current, compare, median)
+ * @param {String} key
+ * @param {Boolean} show
+ * @return {Object} - Redux action
+ */
+const setIVTimeSeriesVisibility = function(key, show) {
+    return {
+     type: 'SET_IV_TIME_SERIES_VISIBILITY',
+     key,
+     show
+    };
+};
+/*
+ * Synchronous action to set the selected variable ID for the IV graph
+ * @param {String} variableID
+ * @return {Object} - Redux action
+ */
+const setCurrentIVVariable = function(variableID) {
+    return {
+        type: 'SET_CURRENT_IV_VARIABLE',
+        variableID
+    };
+};
+
+/*
+ * Synchronous action to set the selected method ID (uniquely identifies the IV time series).
+ * Some variables have more than one IV time series. The method ID distinguishes these
+ * @param {String} methodID
+ * @return {Object} - Redux action
+ */
+const setCurrentIVMethodID = function(methodID) {
+    return {
+        type: 'SET_CURRENT_IV_METHOD_ID',
+        methodID
+    };
+};
+
+/*
+ * Synchronous action sets the date range kind of the IV data.
+ * @param {String} dateRangeKind - represents an ISO 8601 Duration or "custom"
+ * @return {Object} - Redux action
+ */
+const setCurrentIVDateRangeKind = function(dateRangeKind) {
+    return {
+        type: 'SET_CURRENT_IV_DATE_RANGE_KIND',
+        dateRangeKind
+    };
+};
+
+/*
+ * Synchronous action sets the custom date range for the IV graph
+ * @param {Number} startTime - epoch in milliseconds
+ * @param {Number} endTime - epoch in milliseconds
+ * @return {Object} - Redux action
+ */
+const setCustomIVTimeRange = function(startTime, endTime) {
+    return {
+        type: 'SET_CUSTOM_IV_TIME_RANGE',
+        startTime,
+        endTime
+    };
+};
+
+/*
+ * Synchronous action sets the IV graph cursor offset - This is the difference
+ * in cursor position and graph start time, in milliseconds
+ * @param {Number} cursorOffset
+ * @return {Object} - Redux action
+ */
+const setIVGraphCursorOffset = function(cursorOffset) {
+    return {
+        type: 'SET_IV_GRAPH_CURSOR_OFFSET',
+        cursorOffset
+    };
+};
+
+/*
+ * Synchronous action sets the IV graph brush offset - in milliseconds
+ * @param {Number} startOffset - difference between brush start and the start time of the displayed time series
+ * @param {Number} endOffset - difference between the end of the displayed time series and the brush end
+ * @return {Object} - Redux action
+ */
+const setIVGraphBrushOffset = function(startOffset, endOffset) {
+    return {
+        type: 'SET_IV_GRAPH_BRUSH_OFFSET',
+        startOffset,
+        endOffset
+    };
+};
+
+/*
+ * Synchronous action to clear the IV graph brush offset
+ * return {Object} - Redux action
+ */
+const clearIVGraphBrushOffset = function() {
+    return {
+        type: 'CLEAR_IV_GRAPH_BRUSH_OFFSET'
+    };
+};
+
+/*
+ * Synchronous action add the tsRequestKeys to  iv time series loading
+ * @param {Array of String} tsRequestKeys
+ * @return {Object} - Redux actions
+ */
+const addIVTimeSeriesToLoadingKeys = function(tsRequestKeys) {
+    return {
+        type: 'ADD_IV_TIME_SERIES_TO_LOADING_KEYS',
+        tsRequestKeys
+    };
+};
+
+/*
+ * Synchronous action to remove the tsRequestKeys from iv time series loading
+ * @param {Array of String} tsRequestKeys
+ * @return {Object} - Redux action
+ */
+const removeIVTimeSeriesFromLoadingKeys = function(tsRequestKeys) {
+    return {
+        type: 'REMOVE_IV_TIME_SERIES_FROM_LOADING_KEYS',
+        tsRequestKeys
+    };
+};
+
+/*
+ * Synchronous action to set audible play ID
+ * @param {Number} audiblePlayId - id of the timer
+ * @return {Object} - Redux actions
+ */
+const ivTimeSeriesPlayOn = function(audiblePlayId) {
+    return {
+        type: 'IV_TIME_SERIES_PLAY_ON',
+        audiblePlayId
+    };
+};
+
+/*
+ * Synchronous action to remove the audible play ID
+ * @return {Object} - Redux actions
+ */
+const ivTimeSeriesPlayStop = function() {
+    return {
+        type: 'IV_TIME_SERIES_PLAY_STOP'
+    };
+};
+
+/*
+ * Asynchronous Redux action which starts audio play. If the play reaches the end
+ * of the time series it is set back to the beginning
+ * @param {Number} maxCursorOffset - epoch in milliseconds
+ * @return {Function}
+ */
+const startTimeSeriesPlay = function(maxCursorOffset) {
+    return function (dispatch, getState) {
+        let state = getState().timeSeriesState;
+        if (state.cursorOffset == null || state.cursorOffset >= maxCursorOffset) {
+            dispatch(setIVGraphCursorOffset(0));
+        }
+        if (!state.audiblePlayId) {
+            let play = function () {
+                let newOffset = getState().timeSeriesState.cursorOffset + 15 * 60 * 1000;
+                if (newOffset > maxCursorOffset) {
+                    dispatch(ivTimeSeriesPlayStop());
+                } else {
+                    dispatch(setIVGraphCursorOffset(newOffset));
+                }
+            };
+            let playId = window.setInterval(play, 10);
+            dispatch(ivTimeSeriesPlayOn(playId));
+        }
+    };
+};
+
+/*
+* Asynchronous Redux action which stops the audible play
+* @return {Function}
+ */
+const stopTimeSeriesPlay = function() {
+    return function(dispatch, getState) {
+        window.clearInterval(getState().timeSeriesState.audiblePlayId);
+        dispatch(ivTimeSeriesPlayStop());
+    };
+};
+
+export const ivTimeSeriesStateReducer = function(ivTimeSeriesState={}, action) {
+    switch (action.type) {
+        case 'SET_IV_TIME_SERIES_VISIBILITY': {
+            let newVisibility = {};
+            newVisibility[action.key] = action.show;
+            return Object.assign({}, ivTimeSeriesState, {
+                showIVTimeSeries: Object.assign({}, ivTimeSeriesState.showSeries, newVisibility)
+            });
+        }
+
+        case 'SET_CURRENT_IV_VARIABLE':
+            return {
+                ...ivTimeSeriesState,
+                currentIVVariableID: action.variableID
+            };
+
+        case 'SET_CURRENT_IV_METHOD_ID':
+            return {
+                ...ivTimeSeriesState,
+                currentIVMethodID: action.methodID
+            };
+
+        case 'SET_CURRENT_IV_DATE_RANGE_KIND':
+            return {
+                ...ivTimeSeriesState,
+                currentIVDateRangeKind: action.dateRangeKind
+            };
+
+        case 'SET_CUSTOM_IV_TIME_RANGE':
+            return {
+                ...ivTimeSeriesState,
+                customIVTimeRange: {
+                    start: action.startTime,
+                    end: action.endTime
+                }
+            };
+
+        case 'SET_IV_GRAPH_CURSOR_OFFSET':
+            return {
+                ...ivTimeSeriesState,
+                ivGraphCursorOffset: action.cursorOffset
+            };
+
+        case 'SET_IV_GRAPH_BRUSH_OFFSET':
+            return {
+                ...ivTimeSeriesState,
+                ivGraphBrushOffset: {
+                    start: action.startOffset,
+                    end: action.endOffset
+                }
+            };
+
+        case 'CLEAR_IV_GRAPH_BRUSH_OFFSET':
+            return {
+                ...ivTimeSeriesState,
+                ivGraphBrushOffset: undefined
+            };
+
+        default: return ivTimeSeriesState;
+    }
+};
+
+export const Actions = {
+    setIVTimeSeriesVisibility,
+    setCurrentIVVariable,
+    setCurrentIVMethodID,
+    setCurrentIVDateRangeKind,
+    setCustomIVTimeRange,
+    setIVGraphCursorOffset,
+    setIVGraphBrushOffset,
+    clearIVGraphBrushOffset,
+    addIVTimeSeriesToLoadingKeys,
+    removeIVTimeSeriesFromLoadingKeys,
+    ivTimeSeriesPlayOn,
+    ivTimeSeriesPlayStop,
+    startTimeSeriesPlay,
+    stopTimeSeriesPlay
+};
--- assets/src/scripts/store/series-reducer.js
+++ assets/src/scripts/store/series-reducer.js
@@ -22,13 +22,6 @@ const resetTimeSeries = function(series, action) {
     return newSeries;
 };
 
-const addIanaTimeZone = function(series, action) {
-    return {
-        ...series,
-        ianaTimeZone: action.ianaTimeZone
-    };
-};
-
 /*
  * Slice reducer
  */
@@ -37,7 +30,6 @@ export const seriesReducer = function(series={}, action) {
     switch (action.type) {
         case 'ADD_TIME_SERIES_COLLECTION': return addTimeSeriesCollection(series, action);
         case 'RESET_TIME_SERIES': return resetTimeSeries(series, action);
-        case 'LOCATION_IANA_TIME_ZONE_SET': return addIanaTimeZone(series, action);
         default: return series;
     }
 };
