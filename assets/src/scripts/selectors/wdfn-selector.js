1a5a7e223bfd8ddb8cdd73a9c17efd5b1ca8fc1b
Matt Henry <matt.henry@gsa.gov>
2020-08-06 12:22:16 -0400

    rm commented code

--- assets/src/scripts/selectors/wdfn-selector.js
+++ assets/src/scripts/selectors/wdfn-selector.js
@@ -3,11 +3,3 @@ import { createSelector } from 'reselect';
 export const Sites = state => state.wdfn.sites || {};
 export const siteTypes = state => state.wdfn.filters.siteTypes || {};
 export const Filters = state => state.wdfn.filters || {};
-// const siteTypeFilters = state => state.wdfn.filters.siteTypes || {};
-
-// export const SiteTypes = createSelector(
-//   siteTypeFilters,
-//   (siteTypes) => {
-//     return Object.keys(siteTypes).map(k => siteTypes[k] ? k : null);
-//   }
-// );
