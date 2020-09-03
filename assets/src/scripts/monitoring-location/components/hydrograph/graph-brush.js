f8eb7cce6a60b7be074e2e9ad38ed78266db55fa
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-09-03 09:44:32 -0500

    change function order for clarity and remove test files

--- assets/src/scripts/monitoring-location/components/hydrograph/graph-brush.js
+++ assets/src/scripts/monitoring-location/components/hydrograph/graph-brush.js
@@ -15,8 +15,11 @@ import {drawDataLines} from './time-series-lines';
 
 export const drawGraphBrush = function(container, store) {
     let customHandle;
+    let layoutHeight;
+
     const brushed = function() {
         console.log('event ', event.selection)
+        console.log('height ', layoutHeight)
         if (!event.sourceEvent || event.sourceEvent.type === 'zoom') {
             return;
         }
@@ -24,7 +27,9 @@ export const drawGraphBrush = function(container, store) {
         if (event.selection == null) {
             customHandle.attr('display', 'none');
         } else {
-            customHandle.attr('display', null).attr('transform', function(d, i) { return 'translate(' + [event.selection[i], - 100 / 3.3] + ')'; });
+            customHandle.attr('display', null).attr('transform', function(d, index) {
+                return 'translate(' + [event.selection[index], - layoutHeight / 3.3] + ')';
+            });
         }
         const xScale = getBrushXScale('current')(store.getState());
         const brushRange = event.selection || xScale.range();
@@ -73,13 +78,7 @@ export const drawGraphBrush = function(container, store) {
         })
         .call(link(store, (svg, {layout, hydrographBrushOffset, xScale}) => {
             let selection;
-
-            const brushResizePath = function(d) {
-                let e = +(d.type == 'e'),
-                    x = e ? 1 : -1,
-                    y = layout.height / 2;
-                return 'M' + (.5 * x) + ',' + y + 'A6,6 0 0 ' + e + ' ' + (6.5 * x) + ',' + (y + 6) + 'V' + (2 * y - 6) + 'A6,6 0 0 ' + e + ' ' + (.5 * x) + ',' + (2 * y) + 'Z' + 'M' + (2.5 * x) + ',' + (y + 8) + 'V' + (2 * y - 8) + 'M' + (4.5 * x) + ',' + (y + 8) + 'V' + (2 * y - 8);
-            };
+            layoutHeight = layout.height;
 
             const graphBrush = brushX()
                 .on('brush end', brushed);
@@ -89,6 +88,15 @@ export const drawGraphBrush = function(container, store) {
             const group = svg.append('g').attr('class', 'brush')
                 .attr('transform', `translate(${layout.margin.left},${layout.margin.top})`);
 
+            graphBrush.handleSize([1]); // make default handle 1px wide
+            graphBrush.extent([[0, 0], [layout.width - layout.margin.right, layout.height - layout.margin.bottom - layout.margin.top]]);
+
+            const brushResizePath = function(d) {
+                let e = +(d.type == 'e'),
+                    x = e ? 1 : -1,
+                    y = layoutHeight / 2;
+                return 'M' + (.5 * x) + ',' + y + 'A6,6 0 0 ' + e + ' ' + (6.5 * x) + ',' + (y + 6) + 'V' + (2 * y - 6) + 'A6,6 0 0 ' + e + ' ' + (.5 * x) + ',' + (2 * y) + 'Z' + 'M' + (2.5 * x) + ',' + (y + 8) + 'V' + (2 * y - 8) + 'M' + (4.5 * x) + ',' + (y + 8) + 'V' + (2 * y - 8);
+            };
             customHandle = group.selectAll('.handle--custom')
                 .data([{type: 'w'}, {type: 'e'}])
                 .enter().append('path')
@@ -96,9 +104,6 @@ export const drawGraphBrush = function(container, store) {
                 .attr('stroke', '#000')
                 .attr('cursor', 'ew-resize')
                 .attr('d', brushResizePath);
-
-            graphBrush.handleSize([1]); // make default handle 1px wide
-            graphBrush.extent([[0, 0], [layout.width - layout.margin.right, layout.height - layout.margin.bottom - layout.margin.top]]);
 
             // Creates the brush
             group.call(graphBrush);
