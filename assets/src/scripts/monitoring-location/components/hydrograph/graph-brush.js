51fffcbda6dabb6fb17449741ce90341f5561152
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-09-08 11:28:34 -0500

    added comments for functions

--- assets/src/scripts/monitoring-location/components/hydrograph/graph-brush.js
+++ assets/src/scripts/monitoring-location/components/hydrograph/graph-brush.js
@@ -61,8 +61,6 @@ export const drawGraphBrush = function(container, store) {
             svg.append('text')
                 .classed('brush-text-hint', true)
                 .text('drag handles to change timeframe')
-                .attr('text-anchor', 'middle')
-                .attr('font-size', 'smaller')
                 .call(link(store,(elem, layout) => elem.attr('transform', `translate(${(layout.width / 2) + (layout.margin.left / 2) }, ${layout.height + 10})`),
                     getBrushLayout
                 ));
@@ -98,12 +96,17 @@ export const drawGraphBrush = function(container, store) {
             graphBrush.handleSize([1]); // make default handle 1px wide
             graphBrush.extent([[0, 0], [layout.width - layout.margin.right, layout.height - layout.margin.bottom - layout.margin.top]]);
 
+            /* Draws the custom brush handle using an SVG path. The path is drawn twice, once for the handle
+            * on the left hand side, which in d3 brush terms is referred to as 'east' (data type 'e'), and then
+            *  inverted for the right hand custom handle */
             const brushResizePath = function(d) {
-                let e = +(d.type == 'e'),
-                    x = e ? 1 : -1,
+                let east = +(d.type == 'e'),
+                    x = east ? 1 : -1,
                     y = layoutHeight / 2;
-                return 'M' + (.5 * x) + ',' + y + 'A6,6 0 0 ' + e + ' ' + (6.5 * x) + ',' + (y + 6) + 'V' + (2 * y - 6) + 'A6,6 0 0 ' + e + ' ' + (.5 * x) + ',' + (2 * y) + 'Z' + 'M' + (2.5 * x) + ',' + (y + 8) + 'V' + (2 * y - 8) + 'M' + (4.5 * x) + ',' + (y + 8) + 'V' + (2 * y - 8);
+                return 'M' + (.5 * x) + ',' + y + 'A6,6 0 0 ' + east + ' ' + (6.5 * x) + ',' + (y + 6) + 'V' + (2 * y - 6) + 'A6,6 0 0 ' + east + ' ' + (.5 * x) + ',' + (2 * y) + 'Z' + 'M' + (2.5 * x) + ',' + (y + 8) + 'V' + (2 * y - 8) + 'M' + (4.5 * x) + ',' + (y + 8) + 'V' + (2 * y - 8);
             };
+            /* Attaches the custom brush handle to the DOM and binds data placeholders 'w' for the west end (right side)
+            * and 'e' for east end of the brush area */
             customHandle = group.selectAll('.handle--custom')
                 .data([{type: 'w'}, {type: 'e'}])
                 .enter().append('path')
