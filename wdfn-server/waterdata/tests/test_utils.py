88512781f74968aab031ecf2f091777a7a826cf9
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-10-13 10:46:43 -0500

    working to mock get

--- wdfn-server/waterdata/tests/test_utils.py
+++ wdfn-server/waterdata/tests/test_utils.py
@@ -6,10 +6,9 @@ from unittest import TestCase, mock
 
 import requests as r
 
-from flask import Response, request
+from flask import Response
 
 from .. import app
-
 
 from ..utils import construct_url, defined_when, execute_get_request, parse_rdb, set_cookie_for_banner_message
 
@@ -52,6 +51,13 @@ class TestCookieSetting(TestCase):
         with app.test_request_context('/'):
             set_cookie_for_banner_message(self.response)
             self.assertEqual([], self.response.headers.getlist('Set-Cookie'))
+
+    @mock.patch('waterdata.utils.set_cookie_for_banner_message.request.cookies.get', return_value=None)
+    def test_set_cookie_for_banner_message_cookie_not_previously_set(self, mock_cookie_get):
+        app.config['SET_COOKIE_TO_HIDE_BANNER_NOTICES'] = True
+        with app.test_request_context('/'):
+            set_cookie_for_banner_message(self.response)
+            self.assertIn('no-show-banner-message', self.response.headers.getlist('Set-Cookie')[0])
 
 
 class TestGetWaterServicesData(TestCase):
