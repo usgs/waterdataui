58ec106e42e7ac16425ca1fc0ca26b40baa1c81f
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-10-16 15:29:39 -0500

    another version of alias setup

--- assets/rollup.config.js
+++ assets/rollup.config.js
@@ -3,21 +3,37 @@
  * NOTE: This is a CommonJS module so it can be imported by Karma.
  */
 
+const path = require('path');
+
+const alias = require('@rollup/plugin-alias');
 const buble = require('@rollup/plugin-buble');
 const commonjs = require('@rollup/plugin-commonjs');
 const json = require('@rollup/plugin-json');
 const resolve = require('@rollup/plugin-node-resolve');
 const replace = require('@rollup/plugin-replace');
-const { uglify } = require('rollup-plugin-uglify');
+const {uglify} = require('rollup-plugin-uglify');
 
 
 const env = process.env.NODE_ENV || 'development';
 
 const getBundleConfig = function(src, dest) {
+    const hydrographEntries = [
+        {find: 'ml', replacement: path.resolve(__dirname, 'src/scripts/monitoring-location')}
+    ];
+    const networkEntries = [
+        {find: 'network', replacement: path.resolve(__dirname, 'src/scripts/network')}
+    ];
+    const entries = src === 'src/scripts/monitoring-location/index.js' ?  hydrographEntries : networkEntries;
 
-    const configMap = {
+    return {
         input: src,
         plugins: [
+            alias({
+                entries,
+                customResolver: resolve.nodeResolve({
+                    extensions: ['.js', '.json']
+                })
+            }),
             resolve.nodeResolve({
                 mainFields: ['module', 'jsnext', 'main']
             }),
@@ -54,14 +70,6 @@ const getBundleConfig = function(src, dest) {
         },
         treeshake: env === 'production'
     };
-
-    if (src == 'src/scripts/network/index.js'){
-        configMap['external'] = {
-           window: 'window'
-        };
-    }
-
-    return configMap;
 };
 
 module.exports = [
