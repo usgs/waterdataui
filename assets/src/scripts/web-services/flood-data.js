ee2c42462e542a8a36ef5dd01f5fa919ba79d6e1
mbucknell <mbucknell@usgs.gov>
2020-11-13 14:37:16 -0600

    Incorporated changes to make a FIM call to determine whether a site has public FIM data.

--- assets/src/scripts/web-services/flood-data.js
+++ assets/src/scripts/web-services/flood-data.js
@@ -1,11 +1,14 @@
 import {get} from 'ui/ajax';
 import config from 'ui/config';
 
+export const FLOOD_SITES_ENDPOINT = `${config.FIM_GIS_ENDPOINT}sites/MapServer/`;
 export const FLOOD_EXTENTS_ENDPOINT = `${config.FIM_GIS_ENDPOINT}floodExtents/MapServer/`;
 export const FLOOD_BREACH_ENDPOINT = `${config.FIM_GIS_ENDPOINT}breach/MapServer/`;
 export const FLOOD_LEVEE_ENDPOINT = `${config.FIM_GIS_ENDPOINT}suppLyrs/MapServer/`;
+
 const WATERWATCH_URL = config.WATERWATCH_ENDPOINT;
 const FORMAT = 'json';
+
 
 /*
  * Retrieve flood features if any for siteno
@@ -13,12 +16,33 @@ const FORMAT = 'json';
  * @return {Promise} resolves to an array of features for the site
  */
 export const fetchFloodFeatures = function(siteno) {
-    const FIM_QUERY = `${FLOOD_EXTENTS_ENDPOINT}/0/query?where=USGSID+%3D+%27${siteno}%27&outFields=USGSID%2C+STAGE&returnGeometry=false&returnTrueCurves=false&returnIdsOnly=false&returnCountOnly=false&returnZ=false&returnM=falsereturnDistinctValues=false&f=json`;
 
-    return get(FIM_QUERY)
+    const FIM_SITE_QUERY = `${FLOOD_SITES_ENDPOINT}0/query?where=SITE_NO%3D%27${siteno}%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=PUBLIC%2CSITE_NO&returnGeometry=false&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson`;
+    return get(FIM_SITE_QUERY)
         .then((response) => {
             const respJson = JSON.parse(response);
-            return respJson.features ? respJson.features : [];
+
+            const isPublic = respJson.features[0].attributes.Public;
+
+            // If site is public, fetchFloodFeatures
+            // 0 means in review/not public
+            if (isPublic > 0) {
+                const FIM_QUERY = `${FLOOD_EXTENTS_ENDPOINT}/0/query?where=USGSID+%3D+%27${siteno}%27&outFields=USGSID%2C+STAGE&returnGeometry=false&returnTrueCurves=false&returnIdsOnly=false&returnCountOnly=false&returnZ=false&returnM=falsereturnDistinctValues=false&f=json`;
+
+                return get(FIM_QUERY)
+                    .then((response) => {
+                        const respJson = JSON.parse(response);
+                        return respJson.features ? respJson.features : [];
+                    })
+                    .catch(reason => {
+                        console.log(`Unable to get FIM data for ${siteno} with reason: ${reason}`);
+                        return [];
+                    });
+
+            } else {
+                console.log('Found site in review');
+                return Promise.resolve([]);
+            }
         })
         .catch(reason => {
             console.log(`Unable to get FIM data for ${siteno} with reason: ${reason}`);
