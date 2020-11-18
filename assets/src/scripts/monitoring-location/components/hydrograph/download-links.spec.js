398ec8d9c93d35b07f041c9b4514658f25ec84a4
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-11-18 15:39:07 -0600

    changed some existing tests to use service root from global config

--- assets/src/scripts/monitoring-location/components/hydrograph/download-links.spec.js
+++ assets/src/scripts/monitoring-location/components/hydrograph/download-links.spec.js
@@ -31,34 +31,34 @@ describe('createHrefForDownloadOfCompareData', () => {
         }
     };
 
-    // it('will convert a NWIS URL to one compatible with WaterServices download if the period is 7 days', () => {
-    //     const parameterCode = '00060';
-    //     const currentIVDateRange = 'P7D';
-    //     expect(createHrefForDownloadLinks(currentIVDateRange, queryInformation, parameterCode, 'current'))
-    //         .toEqual('https://fakeserviceroot.com/iv/?sites=05370000&parameterCd=00060&&period=P7D&siteStatus=all&format=rdb');
-    // });
-    // it('will convert a NWIS URL one compatible with WaterServices download  if the period is 30 days', () => {
-    //     const parameterCode = '00060';
-    //     const currentIVDateRange = 'P30D';
-    //     expect(createHrefForDownloadLinks(currentIVDateRange, queryInformation, parameterCode, 'current'))
-    //         .toEqual('https://fakeserviceroot.com/iv/?sites=05370000&parameterCd=00060&startDT=2020-10-18T18:33Z&endDT=2020-11-17T19:33Z&siteStatus=all&format=rdb');
-    // });
-    // it('will convert a NWIS URL one compatible with WaterServices download if the period is 1 year', () => {
-    //     const parameterCode = '00060';
-    //     const currentIVDateRange = 'P1Y';
-    //     expect(createHrefForDownloadLinks(currentIVDateRange, queryInformation, parameterCode, 'current'))
-    //         .toEqual('https://fakeserviceroot.com/iv/?sites=05370000&parameterCd=00060&startDT=2019-11-17T19:33Z&endDT=2020-11-17T19:33Z&siteStatus=all&format=rdb');
-    // });
-    // it('will convert a NWIS URL one compatible with WaterServices download if the period is 1 year and parameter code is 00065', () => {
-    //     const parameterCode = '00065';
-    //     const currentIVDateRange = 'P1Y';
-    //     expect(createHrefForDownloadLinks(currentIVDateRange, queryInformation, parameterCode, 'current'))
-    //         .toEqual('https://fakeserviceroot.com/iv/?sites=05370000&parameterCd=00060&startDT=2019-11-17T19:33Z&endDT=2020-11-17T19:33Z&siteStatus=all&format=rdb');
-    // });
-    // it('will convert a NWIS URL one compatible with WaterServices download if the period is 1 year, parameter code is 00065, and type is compare', () => {
-    //     const parameterCode = '00065';
-    //     const currentIVDateRange = 'P1Y';
-    //     expect(createHrefForDownloadLinks(currentIVDateRange, queryInformation, parameterCode, 'compare'))
-    //         .toEqual('https://fakeserviceroot.com/iv/?sites=05370000&startDT=2018-11-17T19:33Z&endDT=2019-11-17T19:33Z&siteStatus=all&format=rdb');
-    // });
+    it('will convert a NWIS URL to one compatible with WaterServices download if the period is 7 days', () => {
+        const parameterCode = '00060';
+        const currentIVDateRange = 'P7D';
+        expect(createHrefForDownloadLinks(currentIVDateRange, queryInformation, parameterCode, 'current'))
+            .toEqual('https://fakeserviceroot.com/iv/?sites=05370000&parameterCd=00060&&period=P7D&siteStatus=all&format=rdb');
+    });
+    it('will convert a NWIS URL one compatible with WaterServices download  if the period is 30 days', () => {
+        const parameterCode = '00060';
+        const currentIVDateRange = 'P30D';
+        expect(createHrefForDownloadLinks(currentIVDateRange, queryInformation, parameterCode, 'current'))
+            .toEqual('https://fakeserviceroot.com/iv/?sites=05370000&parameterCd=00060&startDT=2020-10-18T18:33Z&endDT=2020-11-17T19:33Z&siteStatus=all&format=rdb');
+    });
+    it('will convert a NWIS URL one compatible with WaterServices download if the period is 1 year', () => {
+        const parameterCode = '00060';
+        const currentIVDateRange = 'P1Y';
+        expect(createHrefForDownloadLinks(currentIVDateRange, queryInformation, parameterCode, 'current'))
+            .toEqual('https://fakeserviceroot.com/iv/?sites=05370000&parameterCd=00060&startDT=2019-11-17T19:33Z&endDT=2020-11-17T19:33Z&siteStatus=all&format=rdb');
+    });
+    it('will convert a NWIS URL one compatible with WaterServices download if the period is 1 year and parameter code is 00065', () => {
+        const parameterCode = '00065';
+        const currentIVDateRange = 'P1Y';
+        expect(createHrefForDownloadLinks(currentIVDateRange, queryInformation, parameterCode, 'current'))
+            .toEqual('https://fakeserviceroot.com/iv/?sites=05370000&parameterCd=00060&startDT=2019-11-17T19:33Z&endDT=2020-11-17T19:33Z&siteStatus=all&format=rdb');
+    });
+    it('will convert a NWIS URL one compatible with WaterServices download if the period is 1 year, parameter code is 00065, and type is compare', () => {
+        const parameterCode = '00065';
+        const currentIVDateRange = 'P1Y';
+        expect(createHrefForDownloadLinks(currentIVDateRange, queryInformation, parameterCode, 'compare'))
+            .toEqual('https://fakeserviceroot.com/iv/?sites=05370000&startDT=2018-11-17T19:33Z&endDT=2019-11-17T19:33Z&siteStatus=all&format=rdb');
+    });
 });
