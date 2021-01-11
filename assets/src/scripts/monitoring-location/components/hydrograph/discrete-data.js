50d0ba8c28f9942a1c37e2c0468e70f89a4e85c3
mbucknell <mbucknell@usgs.gov>
2021-01-11 08:32:58 -0600

    Added rendering code for gw levels to the IV graph

--- assets/src/scripts/monitoring-location/components/hydrograph/discrete-data.js
+++ assets/src/scripts/monitoring-location/components/hydrograph/discrete-data.js
@@ -7,9 +7,8 @@ export const drawGroundWaterLevels = function(svg, {levels, xScale, yScale}) {
 
     levels.forEach((level) => {
         group.append('circle')
-            .data(level)
             .attr('r', CIRCLE_RADIUS_SINGLE_PT)
-            .attr('cx', d => xScale(d.dateTime))
-            .attr('cy', d => yScale(d.value));
+            .attr('cx', xScale(level.dateTime))
+            .attr('cy', yScale(level.value));
     });
 };
