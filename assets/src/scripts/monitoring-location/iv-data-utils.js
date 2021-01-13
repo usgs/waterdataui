369a7f74925ed3eb894bd5ee0f742f675a5be3be
Aaron Briggs <abriggs@contractor.usgs.gov>
2021-01-12 20:12:40 -0600

    added check for duplicate F codes

--- assets/src/scripts/monitoring-location/iv-data-utils.js
+++ assets/src/scripts/monitoring-location/iv-data-utils.js
@@ -6,6 +6,46 @@ import config from 'ui/config';
 import {convertCelsiusToFahrenheit} from 'ui/utils';
 
 /*
+* Avoids having two Fahrenheit parameters for monitoring locations that already have both a measured Celsius and
+* Fahrenheit value for corresponding data parameters.
+*  Below is a listing of the known codes and counterparts
+* '00020': '00021' - air temperature C:F
+* '00010': '00011' - water temperature C:F
+* '45589': '45590' - Temperature, internal, within equipment shelter C:F
+* @params {String} parameterCode - five  digit USGS identifier for data sampled at monitoring location
+* @params {Object} - NWISVariable - contains information about the current NWIS 'variables' in the application state
+* @returns {Boolean} - isParameterMatching, true if current parameter code matches a corresponding code for
+* Fahrenheit in the application state, false if it does not.
+ */
+const checkForMeasuredFahrenheitParameters = function(parameterCode, NWISVariables) {
+    let isParameterMatching;
+
+    const allVariableParameterCodes = Object.entries(NWISVariables).map(variable => variable[1].variableCode.value);
+    const celsiusTemperatureCodesWithFahrenheitCounterparts = ['00020', '00010', '45589'];
+    // If the current parameter code is one that could match a measured Fahrenheit parameter, cross reference the
+    // parameter with the known corresponding codes that may match a value in the application state.
+    if (celsiusTemperatureCodesWithFahrenheitCounterparts.includes(parameterCode)) {
+        switch(parameterCode) {
+            case '00020':
+                allVariableParameterCodes.includes('00021')? isParameterMatching = true : isParameterMatching = false;
+                break;
+            case '00010':
+                allVariableParameterCodes.includes('00011')? isParameterMatching = true : isParameterMatching = false;
+                break;
+            case '45589':
+                allVariableParameterCodes.includes('45590')? isParameterMatching = true : isParameterMatching = false;
+                break;
+            default:
+                isParameterMatching = false;
+        }
+    } else {
+        isParameterMatching = false;
+    }
+
+    return isParameterMatching;
+};
+
+/*
 * Converts a Celsius 'variable' from the NWIS system to one we can use to show Fahrenheit.
 * @param {Object} NWISVariable - Has various properties related to descriptions of data
 * @return {Object} NWIS variable converted for use with Fahrenheit
@@ -63,10 +103,12 @@ export const convertCelsiusCollectionsToFahrenheitAndMerge = function(collection
         // For any Celsius parameter codes, clone the application state variables, 'variables and timeSeries'
         // and convert the appropriate properties to Fahrenheit. Then add the cloned objects to the 'collection'
         if (config.TEMPERATURE_PARAMETERS.celsius.includes(parameterCode)) {
-            merge(collection.variables,
-                createConvertedVariable(cloneDeep(collection.variables[variableCode])));
-            merge(collection.timeSeries,
-                createConvertedTimeSeries(cloneDeep(collection.timeSeries[tsRequestKey]), tsRequestKey, parameterCode));
+            if (!checkForMeasuredFahrenheitParameters(parameterCode, collection.variables)) {
+                merge(collection.variables,
+                    createConvertedVariable(cloneDeep(collection.variables[variableCode])));
+                merge(collection.timeSeries,
+                    createConvertedTimeSeries(cloneDeep(collection.timeSeries[tsRequestKey]), tsRequestKey, parameterCode));
+            }
         }
     });
 };
