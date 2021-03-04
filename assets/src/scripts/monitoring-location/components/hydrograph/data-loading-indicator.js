94b2c09a7dbaf2bd7552964beba685b84a57a028
mbucknell <mbucknell@usgs.gov>
2021-03-04 07:50:03 -0600

    Added doc comments for exported function

--- assets/src/scripts/monitoring-location/components/hydrograph/data-loading-indicator.js
+++ assets/src/scripts/monitoring-location/components/hydrograph/data-loading-indicator.js
@@ -4,6 +4,12 @@ import config from 'ui/config';
 import {mediaQuery} from 'ui/utils';
 import {drawLoadingIndicator} from 'd3render/loading-indicator';
 
+/*
+ * Makes the loading indicator visible if isVisible otherwise removes the loading indicator.
+ * The layoutHeight is used to roughly center the spinner within the hydrograph.
+ * @param {Boolean} isVisible
+ * @param {Number} layoutHeight
+ */
 export const showDataLoadingIndicator = function(isVisible, layoutHeight) {
     const container = select('#hydrograph-loading-indicator-container')
         .call(drawLoadingIndicator, {
