47f6ef012062ed6f150f35ff6126e53911e99f0e
Aaron Briggs <abriggs@contractor.usgs.gov>
2021-03-20 08:25:46 -0500

    remove old parameter table

--- assets/src/scripts/monitoring-location/components/hydrograph/parameters.js
+++ assets/src/scripts/monitoring-location/components/hydrograph/parameters.js
@@ -29,13 +29,13 @@ const addRowExpansionControl = function(element, parameter) {
             .text('open')
             .on('click', function(event) {
                 event.stopPropagation();
-                selectAll('.wateralert-row')
+                selectAll('.expansion-container-row')
                     .filter(function() {
-                        return this.id !== `wateralert-row-${parameter.parameterCode}`;
+                        return this.id !== `expansion-container-row-${parameter.parameterCode}`;
                     })
                     .attr('hidden', 'true');
-                select(`#wateralert-row-${parameter.parameterCode}`)
-                    .attr('hidden', select(`#wateralert-row-${parameter.parameterCode}`).attr('hidden') !== null ? null : 'true');
+                select(`#expansion-container-row-${parameter.parameterCode}`)
+                    .attr('hidden', select(`#expansion-container-row-${parameter.parameterCode}`).attr('hidden') !== null ? null : 'true');
             });
     }
 };
@@ -53,6 +53,10 @@ const drawContainingRow = function(store, siteno, element, parameter) {
     return element.append('div')
         .attr('id', `container-row-${parameter.parameterCode}`)
         .attr('class', 'grid-container grid-row-container-row')
+        .attr('ga-on', 'click')
+        .attr('ga-event-category', 'selectTimeSeries')
+        .attr('ga-event-action', `time-series-parmcd-${parameter.parameterCode}`)
+        .attr('role', 'option')
         .call(link(store, (elem, selectedParameterCode) => {
             elem.classed('selected', parameter.parameterCode === selectedParameterCode)
                 .attr('aria-selected', parameter.parameterCode === selectedParameterCode);
@@ -60,9 +64,9 @@ const drawContainingRow = function(store, siteno, element, parameter) {
         .on('click', function() {
             const thisClass = select(this)
                 .attr('class');
-            selectAll('.wateralert-row')
+            selectAll('.expansion-container-row')
                 .attr('hidden', 'true');
-            select(`#wateralert-row-${parameter.parameterCode}`).attr('hidden', null);
+            select(`#expansion-container-row-${parameter.parameterCode}`).attr('hidden', null);
 
             if (!thisClass || !thisClass.includes('selected')) {
                 store.dispatch(setSelectedParameterCode(parameter.parameterCode));
@@ -150,7 +154,6 @@ console.log('parameter ', parameter)
     if(parameter.waterAlert.hasWaterAlert) {
         gridRowInnerWaterAlert.append('div')
             .attr('id', `wateralert-row-${parameter.parameterCode}`)
-            .attr('hidden', true)
             .attr('class', 'grid-col grid-offset-1 wateralert-row')
             .append('a')
             .attr('href', `${config.WATERALERT_SUBSCRIPTION}/?site_no=${siteno}&parm=${parameter.parameterCode}`)
@@ -191,117 +194,13 @@ export const drawSelectionList = function(container, store, siteno) {
         // Add the nested grid rows
         drawTopPeriodOfRecordRow(containerRow, parameter);
         drawRadioButtonRow(store, containerRow, parameter);
-        drawWaterAlertRow(siteno, containerRow, parameter);
-        // Add the expansion container
-
+        // Add the expansion container in nested grid
+        const expansionContainerRow = containerRow.append('div')
+            .attr('id', `expansion-container-row-${parameter.parameterCode}`)
+            .attr('class', 'expansion-container-row')
+            .attr('hidden', 'true');
         // Add the rows nested in the expansion container
-
-    });
-};
-
-/**
- * Draws a table with clickable rows of time series parameter codes. Selecting
- * a row changes the active parameter code.
- */
-export const drawSelectionTable = function(container, store, siteno) {
-    const parameters = getAvailableParameters(store.getState());
-
-    if (!Object.keys(parameters).length) {
-        return;
-    }
-
-    const columnHeaders = ['   ', 'Parameter', 'Period of Record', 'WaterAlert'];
-    const tableContainer = container.append('div')
-        .attr('id', 'select-time-series');
-
-    tableContainer.append('label')
-        .attr('id', 'select-time-series-label')
-        .text('Select a time series');
-    const table = tableContainer.append('table')
-        .classed('usa-table', true)
-        .classed('usa-table--borderless', true)
-        .attr('aria-labelledby', 'select-time-series-label')
-        .attr('tabindex', 0)
-        .attr('role', 'listbox');
-
-    table.append('thead')
-        .append('tr')
-            .selectAll('th')
-            .data(columnHeaders)
-            .enter().append('th')
-                .attr('scope', 'col')
-                .text(d => d);
-    table.append('tbody')
-        .attr('class', 'usa-fieldset')
-        .selectAll('tr')
-        .data(Object.values(parameters))
-        .enter().append('tr')
-        .attr('id', d => `time-series-select-table-row-${d.parameterCode}`)
-        .attr('ga-on', 'click')
-        .attr('ga-event-category', 'selectTimeSeries')
-        .attr('ga-event-action', (d) => `time-series-parmcd-${d.parameterCode}`)
-        .attr('role', 'option')
-        .call(link(store, (trElem, selectedParameterCode) => {
-            trElem
-                .classed('selected', d => d.parameterCode === selectedParameterCode)
-                .attr('aria-selected', d => d.parameterCode === selectedParameterCode);
-        }, getSelectedParameterCode))
-        .on('click', function(event, d) {
-            const thisClass = select(this).attr('class');
-            if (!thisClass || !thisClass.includes('selected')) {
-                store.dispatch(setSelectedParameterCode(d.parameterCode));
-                showDataIndicators(true, store);
-                store.dispatch(retrieveHydrographData(siteno, getInputsForRetrieval(store.getState())))
-                    .then(() => {
-                        showDataIndicators(false, store);
-                    });
-            }
-        })
-        .call(tr => {
-            const paramSelectCol = tr.append('td');
-            paramSelectCol.append('input')
-                .attr('id', d => `time-series-select-radio-button-${d.parameterCode}`)
-                .attr('type', 'radio')
-                .attr('name', 'param-select-radio-input')
-                .attr('class', 'usa-radio__input')
-                .attr('value', d => `${d.parameterCode}`)
-                .call(link(store, (inputElem, selectedParameterCode) => {
-                    inputElem.property('checked', d => d.parameterCode === selectedParameterCode ? true : null);
-                }, getSelectedParameterCode));
-            paramSelectCol.append('label')
-               .attr('class', 'usa-radio__label');
-            const paramCdCol = tr.append('th')
-                .attr('scope', 'row');
-            paramCdCol.append('span')
-                .text(d => d.description)
-                .each(function(d) {
-                    appendInfoTooltip(select(this), `Parameter code: ${d.parameterCode}`);
-                });
-            tr.append('td')
-                .style('white-space', 'nowrap')
-                .text(d => d.periodOfRecord ?
-                    `${d.periodOfRecord.begin_date} to ${d.periodOfRecord.end_date}` : '');
-            tr.append('td')
-                .append('div')
-                .attr('class', 'wateralert-link');
-        });
-
-    // WaterAlert does not support every parameter code, so lets take that into account when adding the links
-    table.selectAll('.wateralert-link').each(function(d) {
-        let selection = select(this);
-
-        let waterAlertCell;
-        if (d.waterAlert.hasWaterAlert) {
-            waterAlertCell = selection.append('a')
-                .attr('href', `${config.WATERALERT_SUBSCRIPTION}/?site_no=${siteno}&parm=${d.waterAlert.subscriptionParameterCode}`);
-        } else {
-            waterAlertCell = selection;
-        }
-        waterAlertCell.attr('class', 'water-alert-cell usa-tooltip')
-            .attr('data-position', 'left')
-            .attr('data-classes', 'width-full tablet:width-auto')
-            .attr('title', d.waterAlert.tooltipText)
-            .text(d.waterAlert.displayText);
+        drawWaterAlertRow(siteno, expansionContainerRow, parameter);
     });
 
     // Activate the USWDS toolTips for WaterAlert subscriptions
