8cbe6449de862665ad26fd785fe955cd70a3d432
Aaron Briggs <abriggs@contractor.usgs.gov>
2021-03-20 07:19:20 -0500

    split code into functions

--- assets/src/scripts/monitoring-location/components/hydrograph/parameters.js
+++ assets/src/scripts/monitoring-location/components/hydrograph/parameters.js
@@ -17,17 +17,158 @@ import {getAvailableParameters} from './selectors/parameter-data';
 
 import {showDataIndicators} from './data-indicator';
 
+/*
+* Helper function that adds the on click open and close functionality. Stopping event propagation is needed to prevent
+* clicks on the containing element from changing this elements behavior.
+* @param {Object} element - the element to add the on click action
+* @param {Object} parameter - Contains details about the current parameter code
+*/
+const addRowExpansionControl = function(element, parameter) {
+    if (parameter.waterAlert.hasWaterAlert) {
+        element
+            .text('open')
+            .on('click', function(event) {
+                event.stopPropagation();
+                selectAll('.wateralert-row')
+                    .filter(function() {
+                        return this.id !== `wateralert-row-${parameter.parameterCode}`;
+                    })
+                    .attr('hidden', 'true');
+                select(`#wateralert-row-${parameter.parameterCode}`)
+                    .attr('hidden', select(`#wateralert-row-${parameter.parameterCode}`).attr('hidden') !== null ? null : 'true');
+            });
+    }
+};
 
+/*
+* Helper function that draws the main containing rows. Note the 'parameter selection' is a nested USWD grid. This section of code
+* creates the main grid rows for each parameter and adds on click functions. Later, another grid will be nested in these rows.
+* @param {Object} Store - The application Redux state
+* @param {String} siteno - A unique identifier for the monitoring location
+* @param {Object} element - The target element to append the row
+* @param {Object} parameter - Contains details about the current parameter code
+* @return {Object} The HTML for the created container row
+*/
+const drawContainingRow = function(store, siteno, element, parameter) {
+    return element.append('div')
+        .attr('id', `container-row-${parameter.parameterCode}`)
+        .attr('class', 'grid-container grid-row-container-row')
+        .call(link(store, (elem, selectedParameterCode) => {
+            elem.classed('selected', parameter.parameterCode === selectedParameterCode)
+                .attr('aria-selected', parameter.parameterCode === selectedParameterCode);
+        }, getSelectedParameterCode))
+        .on('click', function() {
+            const thisClass = select(this)
+                .attr('class');
+            selectAll('.wateralert-row')
+                .attr('hidden', 'true');
+            select(`#wateralert-row-${parameter.parameterCode}`).attr('hidden', null);
 
+            if (!thisClass || !thisClass.includes('selected')) {
+                store.dispatch(setSelectedParameterCode(parameter.parameterCode));
+                showDataIndicators(true, store);
+                store.dispatch(retrieveHydrographData(siteno, getInputsForRetrieval(store.getState())))
+                    .then(() => {
+                        showDataIndicators(false, store);
+                    });
+            }
+        });
+};
 
+/*
+* Helper function that creates the top row of each parameter selection. This row is hidden except on narrow screens
+* and contains the period of record that appears above the parameter description.
+* @param {Object} Element - The target element to append the row
+* @param {Object} parameter - Contains details about the current parameter code
+* @return {Object} The HTML for the grid row
+* */
+const drawTopPeriodOfRecordRow = function(element, parameter) {
+    const gridRowInnerTopPeriodOfRecord = element.append('div')
+        .attr('class', 'grid-row grid-row-inner grid-row-period-of-record');
+    gridRowInnerTopPeriodOfRecord.append('div')
+        .attr('class', 'grid-col grid-offset-1')
+        .text(`${parameter.periodOfRecord.begin_date} to ${parameter.periodOfRecord.end_date}`);
+    const TopPeriodOfRecordRowExpansionControlDiv = gridRowInnerTopPeriodOfRecord.append('div')
+        .attr('class', 'grid-col open-close-top-period-of-record');
+
+    addRowExpansionControl(TopPeriodOfRecordRowExpansionControlDiv, parameter);
+
+    return gridRowInnerTopPeriodOfRecord;
+};
+
+/*
+* Helper function that draws the row containing the radio button and parameter description.
+* @param {Object} Store - The application Redux state
+* @param {Object} Element - The target element to append the row
+* @param {Object} parameter - Contains details about the current parameter code
+* @return {Object} The HTML for the grid row
+*/
+const drawRadioButtonRow = function(store, element, parameter) {
+    const gridRowInnerWithRadioButton = element.append('div')
+        .attr('class', 'grid-row grid-row-inner');
+    const radioButtonDiv = gridRowInnerWithRadioButton.append('div')
+        .attr('class', 'grid-col-1 radio-button__param-select')
+        .append('div')
+        .attr('class', 'usa-radio');
+    radioButtonDiv.append('input')
+        .attr('class', 'usa-radio__input')
+        .attr('id', `radio-${parameter.parameterCode}`)
+        .attr('type', 'radio')
+        .attr('name', 'parameter-selection')
+        .attr('value', `${parameter.parameterCode}`)
+        .call(link(store, (inputElem, selectedParameterCode) => {
+            inputElem.property('checked', parameter.parameterCode === selectedParameterCode ? true : null);
+        }, getSelectedParameterCode));
+    radioButtonDiv.append('label')
+        .attr('class', 'usa-radio__label')
+        .attr('for', `radio-${parameter.parameterCode}`);
+    gridRowInnerWithRadioButton.append('div')
+        .attr('class', 'grid-col-7 description__param-select')
+        .append('div')
+        .text(`${parameter.description}`);
+    gridRowInnerWithRadioButton.append('div')
+        .attr('class', 'grid-col grid-col-auto period-of-record__param-select')
+        .text(`${parameter.periodOfRecord.begin_date} to ${parameter.periodOfRecord.end_date}`);
+    const radioRowExpansionControlDiv = gridRowInnerWithRadioButton.append('div')
+        .attr('class', 'grid-col open-close-radio_button_row');
+    addRowExpansionControl(radioRowExpansionControlDiv, parameter);
+
+    return gridRowInnerWithRadioButton;
+};
+
+/*
+* Helper function that draws a row containing the controls for the WaterAlert subscription.
+* @param {Object} Element - The target element to append the row
+* @param {Object} parameter - Contains details about the current parameter code
+* @return {Object} The HTML for the grid row
+*/
+const drawWaterAlertRow = function(element, parameter) {
+    const gridRowInnerWaterAlert = element.append('div')
+        .attr('class', 'grid-row grid-row-inner');
+
+    if(parameter.waterAlert.hasWaterAlert) {
+        gridRowInnerWaterAlert.append('div')
+            .attr('id', `wateralert-row-${parameter.parameterCode}`)
+            .attr('hidden', true)
+            .attr('class', 'grid-col grid-offset-1 wateralert-row')
+            .text('WaterAlert link');
+    }
+
+    return gridRowInnerWaterAlert;
+};
+
+/*
+* A main function that creates the parameter selection list
+* @param {Object} Store - The application Redux state
+* @param {Object} Element - The target element to append the selection list
+* @param {String} siteno - A unique identifier for the monitoring location
+* */
 export const drawSelectionList = function(container, store, siteno) {
     const parameters = getAvailableParameters(store.getState());
 
     if (!Object.keys(parameters).length) {
         return;
     }
-
-    console.log('parameters ', parameters)
 
     container.append('div')
         .attr('id', 'parameter-selection-container')
@@ -37,98 +178,15 @@ export const drawSelectionList = function(container, store, siteno) {
         .attr('class', 'grid-container');
 
     parameters.forEach(parameter => {
-        const containerRow = selectionList.append('div')
-            .attr('id', `container-row-${parameter.parameterCode}`)
-            .attr('class', 'grid-container grid-row-container-row')
-            .call(link(store, (elem, selectedParameterCode) => {
-                elem.classed('selected', parameter.parameterCode === selectedParameterCode)
-                    .attr('aria-selected', parameter.parameterCode === selectedParameterCode);
-            }, getSelectedParameterCode))
-            .on('click', function() {
-                const thisClass = select(this)
-                    .attr('class');
-                selectAll('.wateralert-row')
-                    .attr('hidden', 'true');
-                select(`#wateralert-row-${parameter.parameterCode}`).attr('hidden', null); // If the clicked row is not expanded, expand it.
+        // Add the main grid rows
+        const containerRow = drawContainingRow(store, siteno, selectionList, parameter);
+        // Add the nested grid rows
+        drawTopPeriodOfRecordRow(containerRow, parameter);
+        drawRadioButtonRow(store, containerRow, parameter);
+        drawWaterAlertRow(containerRow, parameter);
+        // Add the expansion container
 
-                if (!thisClass || !thisClass.includes('selected')) {
-                    store.dispatch(setSelectedParameterCode(parameter.parameterCode));
-                    showDataIndicators(true, store);
-                    store.dispatch(retrieveHydrographData(siteno, getInputsForRetrieval(store.getState())))
-                        .then(() => {
-                            showDataIndicators(false, store);
-                        });
-                }
-
-            });
-
-        const gridRowInnerTopPeriodOfRecord = containerRow.append('div')
-            .attr('class', 'grid-row grid-row-inner grid-row-period-of-record');
-        const gridRowInnerWithRadioButton = containerRow.append('div')
-            .attr('class', 'grid-row grid-row-inner');
-        const gridRowInnerWaterAlert = containerRow.append('div')
-            .attr('class', 'grid-row grid-row-inner');
-
-
-        gridRowInnerTopPeriodOfRecord.append('div')
-            .attr('class', 'grid-col grid-offset-1')
-            .text(`${parameter.periodOfRecord.begin_date} to ${parameter.periodOfRecord.end_date}`);
-        gridRowInnerTopPeriodOfRecord.append('div')
-            .attr('class', 'grid-col open-close-icon__param-select')
-            .text('open');
-
-
-
-        const radioButtonDiv = gridRowInnerWithRadioButton.append('div')
-            .attr('class', 'grid-col-1 radio-button__param-select')
-            .append('div')
-                .attr('class', 'usa-radio');
-        radioButtonDiv.append('input')
-            .attr('class', 'usa-radio__input')
-            .attr('id', `radio-${parameter.parameterCode}`)
-            .attr('type', 'radio')
-            .attr('name', 'parameter-selection')
-            .attr('value', `${parameter.parameterCode}`)
-            .call(link(store, (inputElem, selectedParameterCode) => {
-                inputElem.property('checked', parameter.parameterCode === selectedParameterCode ? true : null);
-            }, getSelectedParameterCode));
-        radioButtonDiv.append('label')
-            .attr('class', 'usa-radio__label')
-            .attr('for', `radio-${parameter.parameterCode}`);
-        gridRowInnerWithRadioButton.append('div')
-            .attr('class', 'grid-col-7 description__param-select')
-            .append('div')
-                .text(`${parameter.description}`);
-        gridRowInnerWithRadioButton.append('div')
-            .attr('class', 'grid-col grid-col-auto period-of-record__param-select')
-            .text(`${parameter.periodOfRecord.begin_date} to ${parameter.periodOfRecord.end_date}`);
-        const rowExpansionControlDiv = gridRowInnerWithRadioButton.append('div')
-            .attr('class', 'grid-col open-close-icon__param-select');
-        if (parameter.waterAlert.hasWaterAlert) {
-            rowExpansionControlDiv
-                .text('open')
-                .on('click', function(event) {
-                event.stopPropagation(); // Don't let clicks on the container div change the settings.
-
-                selectAll('.wateralert-row')
-                    .filter(function() {
-                        return this.id !== `wateralert-row-${parameter.parameterCode}`;
-                    })
-                    .attr('hidden', 'true');
-
-                select(`#wateralert-row-${parameter.parameterCode}`)
-                    .attr('hidden', select(`#wateralert-row-${parameter.parameterCode}`).attr('hidden') !== null ? null : 'true');
-                });
-        }
-
-
-        if(parameter.waterAlert.hasWaterAlert) {
-            gridRowInnerWaterAlert.append('div')
-                .attr('id', `wateralert-row-${parameter.parameterCode}`)
-                .attr('hidden', true)
-                .attr('class', 'grid-col grid-offset-1 wateralert-row')
-                .text('WaterAlert link');
-        }
+        // Add the rows nested in the expansion container
 
     });
 };
