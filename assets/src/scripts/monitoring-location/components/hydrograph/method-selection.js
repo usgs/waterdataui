c3c58bf0ac536d279172e9a99eebd2120f86624e
Aaron Briggs <abriggs@contractor.usgs.gov>
2021-04-01 16:36:48 -0500

    basic checkbox structure set up

--- assets/src/scripts/monitoring-location/components/hydrograph/method-selection.js
+++ assets/src/scripts/monitoring-location/components/hydrograph/method-selection.js
@@ -10,16 +10,34 @@ export const drawSamplingMethodRow = function(container, parameterCode, primaryS
         .attr('id', 'primary-sampling-method-row')
         .attr('class', 'grid-container grid-row-inner');
 
-    gridRowSamplingMethodSelection.append('div')
+    const checkboxContainer = gridRowSamplingMethodSelection.append('div')
         .attr('id', `method-selection-container-${parameterCode}`)
         .attr('class', 'grid-row method-selection-row')
         .append('form')
-            .attr('class', 'usa-form')
-            .append('legend')
-                .attr('class', 'usa-legend')
-                .text('Sampling Methods:');
+            .attr('class', 'usa-form');
+    const checkboxFieldset = checkboxContainer.append('fieldset')
+                .attr('class', 'usa-fieldset');
+    checkboxFieldset.append('legend')
+                    .attr('class', 'usa-legend')
+                    .text('Sampling Methods:');
+    primarySamplingMethods.forEach(method => {
+        console.log('method ', method)
+        const checkboxDiv = checkboxFieldset.append('div')
+            .attr('class', 'usa-checkbox');
+        checkboxDiv.append('input')
+            .attr('id', `checkbox-method-select-${method.methodID}`)
+            .attr('class', 'usa-checkbox__input')
+            .attr('type', 'checkbox')
+            .attr('name', 'method-selection')
+            .attr('value', method.methodID);
+        checkboxDiv.append('label')
+            .attr('class', 'usa-checkbox__label')
+            .attr('for', `checkbox-method-select-${method.methodID}`)
+            .text(method.methodDescription);
 
-    
+    });
+
+
 
     // gridRowSamplingMethodSelection.append('div')
     //     .attr('id', `method-selection-container-${parameterCode}`)
