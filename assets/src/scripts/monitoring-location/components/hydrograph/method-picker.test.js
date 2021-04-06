8a65a49d39a3b9a633166f028e3b99e5c96ac582
Aaron Briggs <abriggs@contractor.usgs.gov>
2021-04-06 18:15:03 -0500

    more tests

--- assets/src/scripts/monitoring-location/components/hydrograph/method-picker.test.js
+++ assets/src/scripts/monitoring-location/components/hydrograph/method-picker.test.js
@@ -100,5 +100,46 @@ describe('monitoring-location/components/hydrograph/method-picker', () => {
             expect(allOptionElements['_groups'][0][0].getAttribute('value')).toBe('90649');
             expect(allOptionElements['_groups'][0][1].getAttribute('value')).toBe('252055');
         });
+
+        it('Expects that there will be a message if some of the methods have no points', () => {
+            const parameterCode = '72019';
+            let store = configureStore({
+                ...TEST_STATE
+            });
+            div.call(drawMethodPicker, parameterCode, store);
+
+            expect(div.select('#no-data-points-note')['_groups'][0][0].innerHTML).toContain('some methods are disabled');
+        });
+
+        it('Expects that there will NOT be a message all of the methods have points', () => {
+            const parameterCode = '72019';
+            let store = configureStore({
+                ...TEST_STATE,
+                hydrographData: {
+                    primaryIVData: {
+                        ...TEST_STATE,
+                        values: {
+                            '90649': {
+                                ...TEST_PRIMARY_IV_DATA.values['90649']
+                            },
+                            '252055': {
+                                points: [
+                                    {value: 25.6, qualifiers: ['E'], dateTime: 1600618500000},
+                                    {value: 26.5, qualifiers: ['P'], dateTime: 1600619400000},
+                                    {value: 25.9, qualifiers: ['P'], dateTime: 1600620300000}
+                                ],
+                                method: {
+                                    methodDescription: 'From multiparameter sonde',
+                                    methodID: '252055'
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+            div.call(drawMethodPicker, parameterCode, store);
+
+            expect(div.select('#no-data-points-note').size()).toBe(0);
+        });
     });
 });
